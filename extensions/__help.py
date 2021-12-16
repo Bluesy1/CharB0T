e6211dbfb8ab57bef4420c85d4e985e3363f418d
Bluesy1 <gkendalfreedman@gmail>
2021-12-16 14:37:59 -0800

    updated stuff

--- extensions/__help.py
+++ extensions/__help.py
@@ -1,3 +1,4 @@
+from hikari.embeds import Embed
 import lightbulb
 
 
@@ -12,17 +13,37 @@ class CustomHelp(lightbulb.BaseHelpCommand):
     async def send_plugin_help(self, context, plugin):
         # Override this method to change the message sent when the help command
         # argument is the name of a plugin.
-        await context.respond(f"{plugin.all_commands}")
+        embed = Embed(title=f"Commands in plugin {plugin.name}")
+        for command in plugin.all_commands:
+            if command is lightbulb.commands.slash.SlashCommand:
+                embed.add_field(command.name,command.options,inline=True)
+            elif command is lightbulb.commands.slash.SlashSubCommand:
+                for subcommand in command.subcommands:
+                    if subcommand is lightbulb.commands.slash.SlashSubCommand:
+                        embed.add_field(subcommand.name,subcommand.options,inline=True)
+                    else:
+                        for subsubcommand in subcommand.subcommands:
+                            embed.add_field(subsubcommand.name,subsubcommand.options,inline=True)
+        await context.respond(embed=embed)
 
     async def send_command_help(self, context, command):
         # Override this method to change the message sent when the help command
         # argument is the name or alias of a command.
-        await context.respond(f"{command.options}")
+        embed = Embed(title=f"Commands in plugin {command.name}")
+        embed.add_field("Options",command.options, inline=True)
+        await context.respond(embed=embed)
 
     async def send_group_help(self, context, group):
         # Override this method to change the message sent when the help command
         # argument is the name or alias of a command group.
-        await context.respond(f"{group.subcommands}")
+        embed = Embed(title=f"Commands in group {group.name}")
+        for command in group.subcommands:
+            if command is lightbulb.commands.slash.SlashSubCommand:
+                embed.add_field(command.name,command.options,inline=True)
+            else:
+                for subcommand in command.subcommands:
+                    embed.add_field(subcommand.name,subcommand.options,inline=True)
+        await context.respond(embed=embed)
 
     """async def object_not_found(self, context, obj):
         # Override this method to change the message sent when help is
