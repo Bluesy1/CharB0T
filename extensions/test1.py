1622b5aff24b1f7d98ab8854bc6772d2a935cd53
Bluesy1 <gkendalfreedman@gmail>
2021-12-29 08:03:46 -0800

    scheduler test

---
+++ extensions/test1.py
@@ -0,0 +1,17 @@
+import lightbulb
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.cron import CronTrigger
+
+
+test_plugin = lightbulb.Plugin("test")
+sched = AsyncIOScheduler()
+sched.start()
+
+
+@sched.scheduled_job(CronTrigger(day_of_week="mon-fri",hour="10",minute="*/1"))
+async def msg1() -> None:
+    await test_plugin.app.rest.create_message(687817008355737606, "Test")
+
+
+def load(bot: lightbulb.BotApp) -> None: bot.add_plugin(test_plugin)
+def unload(bot):bot.remove_plugin(test_plugin)
