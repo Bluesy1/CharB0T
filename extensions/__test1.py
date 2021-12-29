bfc9c4458a02400540562ea547bcf72e0b18ef9a
Bluesy1 <gkendalfreedman@gmail>
2021-12-29 08:46:58 -0800

    updated test

--- extensions/__test1.py
+++ extensions/__test1.py
@@ -8,9 +8,9 @@ sched = AsyncIOScheduler()
 sched.start()
 
 
-@sched.scheduled_job(CronTrigger(day_of_week="wed",hour="11/1",second="*/1"),id="1")
+@sched.scheduled_job(CronTrigger(day_of_week="mon-fri",hour="16/1"),id="1")
 async def msg1() -> None:
-    await test_plugin.app.rest.create_message(687817008355737606, "Test")
+    await test_plugin.app.rest.create_message(687817008355737606, "Market Test")
 
 
 def load(bot: lightbulb.BotApp) -> None: bot.add_plugin(test_plugin)
