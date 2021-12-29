07086457da039cd80c3bde3bec94fb1049d61435
Bluesy1 <gkendalfreedman@gmail>
2021-12-29 08:25:29 -0800

    final test of today

--- extensions/test1.py
+++ extensions/test1.py
@@ -8,7 +8,7 @@ sched = AsyncIOScheduler()
 sched.start()
 
 
-@sched.scheduled_job(CronTrigger(day_of_week="mon-fri",hour="15",second="*/1"))
+@sched.scheduled_job(CronTrigger(day_of_week="wed",hour="11/1",second="*/1"))
 async def msg1() -> None:
     await test_plugin.app.rest.create_message(687817008355737606, "Test")
 
