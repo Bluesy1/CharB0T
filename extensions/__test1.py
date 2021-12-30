22df82606c9e1bb389dd182f026722f56a31edb2
Bluesy1 <gkendalfreedman@gmail.com>
2021-12-29 20:50:41 -0800

    updates

--- extensions/__test1.py
+++ extensions/__test1.py
@@ -2,13 +2,17 @@ import lightbulb
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.cron import CronTrigger
 
+import zoneinfo
+from datetime import datetime
+
 
 test_plugin = lightbulb.Plugin("test")
 sched = AsyncIOScheduler()
 sched.start()
 
+NYC = zoneinfo.ZoneInfo("America/New_York") 
 
-@sched.scheduled_job(CronTrigger(day_of_week="mon-fri",hour="16"),id="1")
+@sched.scheduled_job(CronTrigger(day_of_week="mon-fri",hour="23",minute="52",timezone=NYC),id="1")
 async def msg1() -> None:
     await test_plugin.app.rest.create_message(687817008355737606, "Market Test")
 
