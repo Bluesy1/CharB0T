3855e7b1b7dce10aee58adb6ef93322fce43a866
Bluesy1 <gkendalfreedman@gmail.com>
2022-01-04 16:36:57 -0800

    fixed error

--- CharB0T.py
+++ CharB0T.py
@@ -83,7 +83,7 @@ async def ping(ctx):
     with open('UserInfo.json','wb') as t:
         t.write(fernet.encrypt(json.dumps(df.to_dict()).encode('utf-8')))
     for id in list(df.index):
-        member:guilds.Member = await bot.rest.fetch_member(int(id))
+        member:guilds.Member = await bot.rest.fetch_member(225345178955808768,int(id))
         name = member.display_name
         newname, count = re.subn("(?<=[\[\(])[^\[\]]{2,4}(?=\]\()","",name)
         if newname != name:
