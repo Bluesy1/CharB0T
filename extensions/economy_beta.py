59dd22eef8af2748958f1c0c31a941efb62edf53
Bluesy1 <gkendalfreedman@gmail.com>
2022-01-14 11:13:45 -0800

    floooats

--- extensions/economy_beta.py
+++ extensions/economy_beta.py
@@ -262,7 +262,7 @@ async def work(ctx:lightbulb.Context):
     seconds = int(guild_info[3]);min_gain=int(guild_info[0]);max_gain = int(guild_info[1]);step = int(guild_info[2]);symbol = guild_info[4]
     timeDifference = (currentUse - lastWork) if lastWork is not None else datetime.timedelta(seconds=seconds)
     if timeDifference < datetime.timedelta(seconds=seconds):
-        await ctx.respond(f"🚫 Error: **{ctx.author.mention}** come back <t:{time.mktime((currentUse + timeDifference).timetuple())}:R> to use this command.",flags=EPHEMERAL)
+        await ctx.respond(f"🚫 Error: **{ctx.author.mention}** come back <t:{round(time.mktime((currentUse + timeDifference).timetuple()))}:R> to use this command.",flags=EPHEMERAL)
     elif timeDifference >= datetime.timedelta(seconds=seconds):
         amount = random.randrange(min_gain, max_gain+1, step) #generates random number from min to max inclusive, in incrememnts of step, the +1 to make it inclusive
         balance += lastamount
