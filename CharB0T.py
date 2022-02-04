2405d9814a24a1598143110853011439c2de4ebf
Bluesy1 <gkendalfreedman@gmail.com>
2022-02-04 12:36:51 -0800

    Updated/reformatted Char1/Kethran
    Signed-off-by: Bluesy1 <gkendalfreedman@gmail.com>

--- CharB0T.py
+++ CharB0T.py
@@ -1,4 +1,4 @@
-#pylint: disable=invalid-name
+# pylint: disable=invalid-name
 import json
 import os
 import sys
@@ -14,6 +14,7 @@ from hikari.intents import Intents
 from hikari.presences import Activity, ActivityType
 from lightbulb import commands
 
+
 def main():
     """Main"""
     global retries
@@ -21,28 +22,27 @@ def main():
         import uvloop  # pylint: disable=import-outside-toplevel
         uvloop.install()
 
-    with open('bottoken.json') as file:
-        token = json.load(file)['Token']
+    token = json.load(open('bottoken.json'))['Token']
     # Instantiate a Bot instance
     bot = lightbulb.BotApp(token=token, prefix="!", help_slash_command=True,
                            owner_ids=[225344348903047168, 363095569515806722], logs={
-                               "version": 1,
-                               "incremental": True,
-                               "loggers": {
-                                   "hikari": {"level": "INFO"},
-                                   "hikari.ratelimits": {"level": "TRACE_HIKARI"},
-                                   "lightbulb": {"level": "INFO"},
-                               },}, case_insensitive_prefix_commands=True, intents=Intents.ALL)
+            "version": 1,
+            "incremental": True,
+            "loggers": {
+                "hikari": {"level": "INFO"},
+                "hikari.ratelimits": {"level": "TRACE_HIKARI"},
+                "lightbulb": {"level": "INFO"},
+            }, }, case_insensitive_prefix_commands=True, intents=Intents.ALL)
 
     @bot.command()
     @lightbulb.option("module", "module to reload", required=True)
     @lightbulb.add_checks(lightbulb.owner_only)
     @lightbulb.command("reload", "reloads extensions")
     @lightbulb.implements(commands.PrefixCommand)
-    async def reload(ctx) -> None:#pylint: disable=unused-variable
+    async def reload(ctx) -> None:  # pylint: disable=unused-variable
         """Live Module Reloading Command"""
         # Send a message to the channel the command was used in
-        bot.reload_extensions("extensions."+ctx.options.module)
+        bot.reload_extensions("extensions." + ctx.options.module)
         await ctx.respond("Reloaded!")
 
     @bot.command()
@@ -50,10 +50,10 @@ def main():
     @lightbulb.add_checks(lightbulb.owner_only)
     @lightbulb.command("load", "loads given extensions")
     @lightbulb.implements(commands.PrefixCommand)
-    async def load(ctx) -> None:#pylint: disable=unused-variable
+    async def load(ctx) -> None:  # pylint: disable=unused-variable
         """Live Module Loading Command"""
         # Send a message to the channel the command was used in
-        bot.load_extensions("extensions."+ctx.options.module)
+        bot.load_extensions("extensions." + ctx.options.module)
         await ctx.respond("Loaded!")
 
     @bot.command()
@@ -61,20 +61,20 @@ def main():
     @lightbulb.add_checks(lightbulb.owner_only)
     @lightbulb.command("unload", "unloads given extensions")
     @lightbulb.implements(commands.PrefixCommand)
-    async def unload(ctx) -> None:#pylint: disable=unused-variable
+    async def unload(ctx) -> None:  # pylint: disable=unused-variable
         """Live Module Unloading Command"""
         # Send a message to the channel the command was used in
-        bot.unload_extensions("extensions."+ctx.options.module)
+        bot.unload_extensions("extensions." + ctx.options.module)
         await ctx.respond("Unloaded!")
 
     @bot.command()
     @lightbulb.add_checks(lightbulb.owner_only)
     @lightbulb.command("ping", "Checks that the bot is alive")
     @lightbulb.implements(commands.PrefixCommand)
-    async def ping(ctx):#pylint: disable=unused-variable
+    async def ping(ctx):  # pylint: disable=unused-variable
         """Ping Command TO Check Bot Is Alive"""
         await ctx.event.message.delete()
-        await ctx.respond(f"Pong! Latency: {bot.heartbeat_latency*1000:.2f}ms")
+        await ctx.respond(f"Pong! Latency: {bot.heartbeat_latency * 1000:.2f}ms")
 
     bot.load_extensions_from("extensions")
 
@@ -84,10 +84,12 @@ def main():
     retries = 0
     scheduler = BackgroundScheduler()
     scheduler.start()
+
     def remove_retry():
         """Removes a Retry"""
         global retries
         retries -= 1
+
     try:
         bot.run(activity=Activity(name="over the server", type=ActivityType.WATCHING))
     except KeyboardInterrupt:
@@ -100,16 +102,16 @@ def main():
         traceback.print_exc()
         sys.exit()
     except:
-        if retries<11:
+        if retries < 11:
             sleep(10)
-            retries+=1
-            scheduler.add_job(remove_retry,DateTrigger(datetime.utcnow() + timedelta(minutes=30)))
+            retries += 1
+            scheduler.add_job(remove_retry, DateTrigger(datetime.utcnow() + timedelta(minutes=30)))
             print(f"Bot Closed, Trying to restart, try {retries}/10")
             main()
         else:
             traceback.print_exc()
             sys.exit()
-    
+
 
 if __name__ == "__main__":
     main()
--- Kethran.py
+++ Kethran.py
@@ -1,11 +1,10 @@
-from datetime import datetime, timedelta
 import json
 import os
 import random
-import re
 import sys
 import time
 import traceback
+from datetime import datetime, timedelta
 
 import hikari
 import lightbulb
@@ -14,89 +13,64 @@ from apscheduler.triggers.cron import CronTrigger
 from apscheduler.triggers.date import DateTrigger
 from lightbulb import commands
 
-def main():#pylint: disable = too-many-statements
+
+def main():  # pylint: disable = too-many-statements
     global retries
     """Main"""
     if os.name != "nt":
-        import uvloop #pylint: disable=import-outside-toplevel
+        import uvloop  # pylint: disable=import-outside-toplevel
         uvloop.install()
 
-    with open("KethranToken.json") as file:
-        token = json.load(file)['Token']
-    regex = r"kethran"
+    token = json.load(open("KethranToken.json"))['Token']
 
     bot = lightbulb.BotApp(token=token, prefix="k", help_class=None,
-                           owner_ids=363095569515806722, case_insensitive_prefix_commands=True)
-    sched = AsyncIOScheduler()
-    sched.start()
-
+                           owner_ids=[363095569515806722], case_insensitive_prefix_commands=True)
+    scheduler = AsyncIOScheduler()
+    scheduler.start()
+    bot.d.responses = [
+        "Bork!", "Bork!", "Bork!", "Bork!", "Bork!",
+        "WOOF!", "WOOF!", "WOOF!", "WOOF!",
+        "AROOOF!", "AROOOF!", "AROOOF!", "AROOOF!",
+        "HOOOWL!", "HOOOWL!", "HOOOWL!", "HOOOWL!", "HOOOWL!",
+        "Aroof", "Aroof", "Aroof", "Aroof",
+        "*Sniff Sniff Sniff*", "*Sniff Sniff Sniff*", "*Sniff Sniff Sniff*", "*Sniff Sniff Sniff*",
+        "*Does not care*", "*Does not care*", "*Does not care*", "*Does not care*",
+        "*I want scritches*", "*I want scritches*", "*I want scritches*",
+        "*I want scritches*", "*I want scritches*",
+        "*is busy*", "*is busy*", "*is busy*", "*is busy*",
+        "snuffles", "snuffles", "snuffles", "snuffles",
+        "snuffles apologetically", "snuffles apologetically",
+        "snuffles apologetically", "snuffles apologetically",
+        "sits", "sits", "sits", "sits",
+        "lays down", "lays down", "lays down", "lays down",
+        "chuffs", "chuffs", "chuffs", "chuffs",
+        "pants", "pants", "pants", "pants",
+        "plays dead", "plays dead", "plays dead", "plays dead",
+        "gives paw", " gives paw", "gives paw", "gives paw",
+        "rolls over", "rolls over", "rolls over", "rolls over",
+        "is adorable", "is adorable", "is adorable", "is adorable",
+        "whimpers", "whimpers", "whimpers", "whimpers",
+        "licks face", " licks face", "licks face", "licks face",
+        "*Sits*", "*Sits*", "*Sits*", "*Sits*",
+        "*is adorable*", "*is adorable*", "*is adorable*", "*is adorable*",
+        "pants", "pants", "pants", "pants",
+        "**HOLY SHIT!** \n**HOLY SHIT!**\n**HOLY SHIT!**",
+    ]
+    bot.d.roll_error = ("Error invalid argument: specified dice can only be d2s, d4s, d6s, d8s, d10s, d12s, d20s, "
+                        "or d100s, or if a constant modifier must be a perfect integer, positive or negative, "
+                        "connexted with `+`, and no spaces.")
 
     @bot.listen(hikari.GuildMessageCreateEvent)
-    async def on_guild_message(event: hikari.GuildMessageCreateEvent): #pylint: disable=unused-variable, too-many-branches, too-many-statements
+    async def on_guild_message(event: hikari.GuildMessageCreateEvent):  # pylint: disable=unused-variable
         """Checks guild messages in correct channels for regex trigger"""
         if event.is_human:
             if event.channel_id in [901325983838244865, 878434694713188362]:
-                if re.search(regex, event.content, re.IGNORECASE|re.MULTILINE) is not None:
-                    rand = random.randint(1, 100)
-                    if rand < 4:
-                        await event.get_channel().send("Bork!", reply=event.message_id, mentions_reply=True)
-                    elif rand < 8:
-                        await event.get_channel().send("WOOF!", reply=event.message_id, mentions_reply=True)
-                    elif rand < 12:
-                        await event.get_channel().send("AROOOF!", reply=event.message_id, mentions_reply=True)
-                    elif rand < 16:
-                        await event.get_channel().send("HOOOWL!", reply=event.message_id, mentions_reply=True)
-                    elif rand < 20:
-                        await event.get_channel().send("Aroof", reply=event.message_id, mentions_reply=True)
-                    elif rand < 24:
-                        await event.get_channel().send("*Sniff Sniff Sniff*",
-                                                       reply=event.message_id, mentions_reply=True)
-                    elif rand < 28:
-                        await event.get_channel().send("*Does not care*", reply=event.message_id, mentions_reply=True)
-                    elif rand < 32:
-                        await event.get_channel().send("*I want scritches*",
-                                                       reply=event.message_id, mentions_reply=True)
-                    elif rand < 36:
-                        await event.get_channel().send("*is busy*", reply=event.message_id, mentions_reply=True)
-                    elif rand < 40:
-                        await event.get_channel().send("snuffles", reply=event.message_id, mentions_reply=True)
-                    elif rand < 44:
-                        await event.get_channel().send("snuffles apologetically",
-                                                       reply=event.message_id, mentions_reply=True)
-                    elif rand < 48:
-                        await event.get_channel().send("sits", reply=event.message_id, mentions_reply=True)
-                    elif rand < 52:
-                        await event.get_channel().send("lays down", reply=event.message_id, mentions_reply=True)
-                    elif rand < 56:
-                        await event.get_channel().send("chuffs", reply=event.message_id, mentions_reply=True)
-                    elif rand < 60:
-                        await event.get_channel().send("pants", reply=event.message_id, mentions_reply=True)
-                    elif rand < 64:
-                        await event.get_channel().send("plays dead", reply=event.message_id, mentions_reply=True)
-                    elif rand < 68:
-                        await event.get_channel().send("gives paw", reply=event.message_id, mentions_reply=True)
-                    elif rand < 72:
-                        await event.get_channel().send("rolls over", reply=event.message_id, mentions_reply=True)
-                    elif rand < 76:
-                        await event.get_channel().send("is adorable", reply=event.message_id, mentions_reply=True)
-                    elif rand < 80:
-                        await event.get_channel().send("whimpers", reply=event.message_id, mentions_reply=True)
-                    elif rand < 84:
-                        await event.get_channel().send("licks face", reply=event.message_id, mentions_reply=True)
-                    elif rand < 88:
-                        await event.get_channel().send("*Sits*", reply=event.message_id, mentions_reply=True)
-                    elif rand < 92:
-                        await event.get_channel().send("*is adorable*", reply=event.message_id, mentions_reply=True)
-                    elif rand < 96:
-                        await event.get_channel().send("pants", reply=event.message_id, mentions_reply=True)
-                    elif rand <= 100:
-                        message = await event.get_channel().send("**HOLY SHIT!**",
-                                                                 reply=event.message_id, mentions_reply=True)
-                        message = await event.get_channel().send("**HOLY SHIT!**", reply=message.id)
-                        await event.get_channel().send("**HOLY SHIT!**", reply=message.id)
+                if "kethran" in event.content.lower():
+                    await event.get_channel().send(
+                        bot.d.responses[random.randint(0, 99)], reply=event.message_id, mentions_reply=True)
 
     @bot.listen(hikari.DMMessageCreateEvent)
-    async def on_dm_message(event: hikari.DMMessageCreateEvent): #pylint: disable=unused-variable
+    async def on_dm_message(event: hikari.DMMessageCreateEvent):  # pylint: disable=unused-variable
         """Checks for DMs from a specific user to forward"""
         if event.is_human and event.content is not None and event.author_id == 184524255197659136:
             channel = await bot.rest.fetch_channel(878434694713188362)
@@ -106,7 +80,7 @@ def main():#pylint: disable = too-many-statements
     @lightbulb.option("dice", "dice to roll", required=True)
     @lightbulb.command("roll", "diceroller")
     @lightbulb.implements(commands.PrefixCommand)
-    async def roll(ctx: lightbulb.Context): #pylint: disable=unused-variable
+    async def roll(ctx: lightbulb.Context):  # pylint: disable=unused-variable
         """Dice roller"""
         arg = ctx.options.dice
         if "+" in arg:
@@ -119,8 +93,8 @@ def main():#pylint: disable = too-many-statements
             allowed_dice = [2, 4, 6, 8, 10, 12, 20, 100]
             for die in dice:
                 if 'd' in die:
-                    if int(die[die.find('d')+1:]) not in allowed_dice:
-                        await ctx.respond("Error invalid argument: specified dice can only be d2s, d4s, d6s, d8s, d10s, d12s, d20s, or d100s, or if a constant modifier must be a perfect integer, positive or negative, connexted with `+`, and no spaces.")#pylint: disable=line-too-long
+                    if int(die[die.find('d') + 1:]) not in allowed_dice:
+                        await ctx.respond(bot.d.roll_error)
                         return
                     try:
                         num_rolls = int(die[:die.find('d')])
@@ -128,87 +102,37 @@ def main():#pylint: disable = too-many-statements
                         num_rolls = 1
                     i = 1
                     while i <= num_rolls:
-                        roll = random.randint(1, int(die[die.find('d') + 1:]))
-                        rolls.append(roll)
-                        sums += roll
+                        roll1 = random.randint(1, int(die[die.find('d') + 1:]))
+                        rolls.append(roll1)
+                        sums += roll1
                         i += 1
                 else:
                     try:
                         rolls.append(int(die))
                         sums += int(die)
                     except ValueError:
-                        await ctx.respond("Error invalid argument: specified dice can only be d2s, d4s, d6s, d8s, d10s, d12s, d20s,  or d100s, or if a constant modifier must be a perfect integer, positive or negative, connexted with `+`, and no spaces.", reply=True)#pylint: disable=line-too-long
+                        await ctx.respond(bot.d.roll_error,reply=True)
                         return
             output = '`'
-            for roll in rolls:
-                output += str(roll) + ', '
+            for roll1 in rolls:
+                output += str(roll1) + ', '
             output = output[:-2]
             await ctx.respond(f"Kethran rolled `{arg}` got {output}` for a total value of: {str(sums)}", reply=True)
-        except Exception:#pylint: disable=broad-except
-            await ctx.respond("Error invalid argument: specified dice can only be d2s, d4s, d6s, d8s, d10s, d12s, d20s,  or d100s, or if a constant modifier must be a perfect integer, positive or negative, connexted with `+`, and no spaces.", reply=True)#pylint: disable=line-too-long
+        except Exception:  # pylint: disable=broad-except
+            await ctx.respond(bot.d.roll_error, reply=True)
 
-    @sched.scheduled_job(CronTrigger(day_of_week="sat", hour="1"), id="1")
-    async def friday_5() -> None: #pylint: disable=unused-variable, too-many-branches, too-many-statements
-        """IDK, its a thing"""
-        rand = random.randint(1, 100)
-        if rand < 4:
-            await bot.rest.create_message(878434694713188362, "Bork!")
-        elif rand < 8:
-            await bot.rest.create_message(878434694713188362, "WOOF!")
-        elif rand < 12:
-            await bot.rest.create_message(878434694713188362, "AROOOF!")
-        elif rand < 16:
-            await bot.rest.create_message(878434694713188362, "HOOOWL!")
-        elif rand < 20:
-            await bot.rest.create_message(878434694713188362, "Aroof")
-        elif rand < 24:
-            await bot.rest.create_message(878434694713188362, "*Sniff Sniff Sniff*")
-        elif rand < 28:
-            await bot.rest.create_message(878434694713188362, "*Does not care*")
-        elif rand < 32:
-            await bot.rest.create_message(878434694713188362, "*I want scritches*")
-        elif rand < 36:
-            await bot.rest.create_message(878434694713188362, "*is busy*")
-        elif rand < 40:
-            await bot.rest.create_message(878434694713188362, "snuffles")
-        elif rand < 44:
-            await bot.rest.create_message(878434694713188362, "snuffles apologetically")
-        elif rand < 48:
-            await bot.rest.create_message(878434694713188362, "sits")
-        elif rand < 52:
-            await bot.rest.create_message(878434694713188362, "lays down")
-        elif rand < 56:
-            await bot.rest.create_message(878434694713188362, "chuffs")
-        elif rand < 60:
-            await bot.rest.create_message(878434694713188362, "pants")
-        elif rand < 64:
-            await bot.rest.create_message(878434694713188362, "plays dead")
-        elif rand < 68:
-            await bot.rest.create_message(878434694713188362, "gives paw")
-        elif rand < 72:
-            await bot.rest.create_message(878434694713188362, "rolls over")
-        elif rand < 76:
-            await bot.rest.create_message(878434694713188362, "is adorable")
-        elif rand < 80:
-            await bot.rest.create_message(878434694713188362, "whimpers")
-        elif rand < 84:
-            await bot.rest.create_message(878434694713188362, "licks face")
-        elif rand < 88:
-            await bot.rest.create_message(878434694713188362, "*Sits*")
-        elif rand < 92:
-            await bot.rest.create_message(878434694713188362, "*is adorable*")
-        elif rand < 96:
-            await bot.rest.create_message(878434694713188362, "pants")
-        elif rand <= 100:
-            message = await bot.rest.create_message(878434694713188362, "**HOLY SHIT!**")
-            message = await bot.rest.create_message(878434694713188362, "**HOLY SHIT!**", reply=message.id)
-            await bot.rest.create_message(878434694713188362, "**HOLY SHIT!**", reply=message.id)
+    @scheduler.scheduled_job(CronTrigger(day_of_week="sat", hour="1"), id="1")
+    async def friday_5() -> None:  # pylint: disable=unused-variable
+        """IDK, it's a thing"""
+        await bot.rest.create_message(878434694713188362, bot.d.responses[random.randint(0, 99)])
 
     retries = 0
+
     def remove_retry():
         """Removes a Retry"""
         global retries
         retries -= 1
+
     try:
         bot.run()
     except KeyboardInterrupt:
@@ -221,15 +145,16 @@ def main():#pylint: disable = too-many-statements
         traceback.print_exc()
         sys.exit()
     except:
-        if retries<11:
+        if retries < 11:
             time.sleep(10)
-            retries+=1
-            sched.add_job(remove_retry,DateTrigger(datetime.utcnow() + timedelta(minutes=30)))
+            retries += 1
+            scheduler.add_job(remove_retry, DateTrigger(datetime.utcnow() + timedelta(minutes=30)))
             print(f"Bot Closed, Trying to restart, try {retries}/10")
             main()
         else:
             traceback.print_exc()
             sys.exit()
+
 
 if __name__ == "__main__":
     main()
