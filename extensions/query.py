e72a1825a4f359228be9601ff7e723ad7c44c939
Bluesy1 <gkendalfreedman@gmail.com>
2022-02-04 16:15:17 -0800

    Updated linter, linted query.py
    Signed-off-by: Bluesy1 <gkendalfreedman@gmail.com>

--- extensions/query.py
+++ extensions/query.py
@@ -4,24 +4,33 @@ import time
 import lightbulb
 from lightbulb import commands
 
-def Punished(context):
-        roles = context.member.role_ids
-        for role in roles:
-            if role in [684936661745795088,676250179929636886]:
-                return False
-        #if context.author.id == 920994579342311465: return False
-        return True 
+
+def punished(context):
+    roles = context.member.role_ids
+    for role in roles:
+        if role in [684936661745795088, 676250179929636886]:
+            return False
+    return True
+
 
 QueryPlugin = lightbulb.Plugin("QueryPlugin")
 
+
 @QueryPlugin.command
-@lightbulb.add_checks(lightbulb.Check(Punished))
-@lightbulb.command("time", "display's charlie's time",guilds=[225345178955808768])
+@lightbulb.add_checks(lightbulb.Check(punished))
+@lightbulb.command("time", "display's charlie's time", guilds=[225345178955808768])
 @lightbulb.implements(commands.SlashCommand)
 async def command(ctx):
     os.environ['TZ'] = 'US/Eastern'
     time.tzset()
     await ctx.respond("Charlie's time is: " + time.strftime('%X %x %Z'))
 
-def load(bot):bot.add_plugin(QueryPlugin)
-def unload(bot):bot.remove_plugin(QueryPlugin)
+
+def load(bot):
+    """Loads the plugin"""
+    bot.add_plugin(QueryPlugin)
+
+
+def unload(bot):
+    """Unloads the plugin"""
+    bot.remove_plugin(QueryPlugin)
