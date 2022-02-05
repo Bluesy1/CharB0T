10603e201093fcb3b84ef3e0688d66e940ba8ca2
Bluesy1 <gkendalfreedman@gmail.com>
2022-02-04 16:01:38 -0800

    Linted key.py
    Signed-off-by: Bluesy1 <gkendalfreedman@gmail.com>

--- key.py
+++ key.py
@@ -1,19 +1,18 @@
 from cryptography.fernet import Fernet
-import json
 
-with open('filekey.key','rb') as file:
+with open('filekey.key', 'rb') as file:
     key = file.read()
 
-#this opens your json and reads its data into a new variable called 'data'
-with open('tickets.json','rb') as f:
+# this opens your json and reads its data into a new variable called 'data'
+with open('tickets.json', 'rb') as f:
     data = f.read()
 
-#this encrypts the data read from your json and stores it in 'encrypted'
+# this encrypts the data read from your json and stores it in 'encrypted'
 fernet = Fernet(key)
 encrypted = fernet.encrypt(data)
 
-#print(json.loads(encrypted))
-#this writes your new, encrypted data into a new JSON file
-with open('tickets.json','wb') as f:
+# print(json.loads(encrypted))
+# this writes your new, encrypted data into a new JSON file
+with open('tickets.json', 'wb') as f:
     f.write(encrypted)
-    #json.dump(json.loads(encrypted),f)
+    # json.dump(json.loads(encrypted),f)
