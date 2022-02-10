6d8611e3171b099e1e4536cad806d8a587b8f25e
Bluesy1 <gkendalfreedman@gmail.com>
2022-02-10 08:44:14 -0800

    Trying something new
    Signed-off-by: Bluesy1 <gkendalfreedman@gmail.com>

--- extensions/__button_test.py
+++ extensions/__button_test.py
@@ -58,6 +58,11 @@ class ButtonTestOne(tungsten.Components):
             ]
         ]
         kwargs["button_group"] = tungsten.ButtonGroup(button_rows)
+        if not kwargs["allowed_ids"]:
+            kwargs["allowed_ids"] = [args[0].author.id]
+        else:
+            if args[0].author.id not in kwargs["allowed_ids"]:
+                kwargs["allowed_ids"].append(args[0].author.id)
         # Actually creates object
         super().__init__(*args, **kwargs)
 
