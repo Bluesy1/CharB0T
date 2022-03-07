77e653fa175e3d13dc5b6cb74a34da22178f8681
Bluesy <gkendalfreedman@gmail.com>
2022-03-07 09:14:26 -0800

    Rewriting things
    Signed-off-by: Bluesy <gkendalfreedman@gmail.com>

--- extensions/admin.py
+++ extensions/admin.py
@@ -1,8 +1,5 @@
 # coding=utf-8
-import asyncio
 import json
-import os
-import time
 from datetime import datetime, timezone
 
 import lightbulb
@@ -10,9 +7,6 @@ from discord import Embed
 from discord.ext import commands
 from discord.ext.commands import Cog, Context
 from hikari.embeds import Embed
-from hikari.events.interaction_events import InteractionCreateEvent
-from hikari.interactions.base_interactions import ResponseType
-from hikari.messages import ButtonStyle
 from lightbulb import SlashCommandGroup, SlashSubGroup, SlashContext, SlashSubCommand
 from lightbulb.checks import has_roles
 
@@ -27,115 +21,6 @@ class Admin(Cog):
 
     def cog_check(self, ctx: Context) -> bool:
         return any(role.id in (338173415527677954, 253752685357039617, 225413350874546176) for role in ctx.author.roles)
-
-
-
-
-
-@AdminPlugin.command
-@lightbulb.command(
-    "maketime", "makes a unix time constructor for use with discord's unix time feature",
-    guilds=[225345178955808768])
-@lightbulb.implements(commands.SlashCommand)
-async def command(ctx):  # pylint: disable=too-many-statements
-    """Command that makes a unix time constructor for use with discord's unix time feature"""
-    time_menu = AdminPlugin.bot.rest.build_action_row()
-    (time_menu.add_select_menu("Editing")
-     .add_option("Done", "Done").set_emoji("✅").set_description("Use this to get the final options").add_to_menu()
-     .add_option("+30 minutes", "+m").set_emoji("➡️").set_description(
-        "Use this to increase the time by 30 minutes").add_to_menu()
-     .add_option("-30 minutes", "-m").set_emoji("⬅️").set_description(
-        "Use this to decrease the time by 30 minutes").add_to_menu()
-     .add_option("+1 hour", "+h").set_emoji("➡️").set_description(
-        "Use this to increase the time by 1 hour").add_to_menu()
-     .add_option("-1 hour", "-h").set_emoji("◀️").set_description(
-        "Use this to decrease the time by 1 hour").add_to_menu()
-     .add_option("-1 day", "-d").set_emoji("⬅️").set_description("Use this to decrease the time by 1 day").add_to_menu()
-     .add_option("+1 day", "+d").set_emoji("▶️").set_description("Use this to increase the time by 1 day").add_to_menu()
-     .add_option("+1 week", "+w").set_emoji("➡️").set_description(
-        "Use this to increase the time by 1 week").add_to_menu()
-     .add_option("-1 week", "-w").set_emoji("◀️").set_description(
-        "Use this to decrease the time by 1 week").add_to_menu()
-     .add_to_container()
-     )
-    add_buttons = AdminPlugin.bot.rest.build_action_row()
-    add_buttons.add_button(ButtonStyle.SUCCESS, "+w").set_label("+1 week").set_emoji("▶️").add_to_container()
-    add_buttons.add_button(ButtonStyle.SUCCESS, "+d").set_label("+1 day").set_emoji("▶️").add_to_container()
-    add_buttons.add_button(ButtonStyle.SUCCESS, "+h").set_label("+1 hour").set_emoji("▶️").add_to_container()
-    add_buttons.add_button(ButtonStyle.SUCCESS, "+m").set_label("+30 minutes").set_emoji("▶️").add_to_container()
-    add_buttons.add_button(ButtonStyle.PRIMARY, "Done").set_label("Done").set_emoji("✅").add_to_container()
-    sub_buttons = AdminPlugin.bot.rest.build_action_row()
-    sub_buttons.add_button(ButtonStyle.DANGER, "-w").set_label("-1 week").set_emoji("◀️").add_to_container()
-    sub_buttons.add_button(ButtonStyle.DANGER, "-d").set_label("-1 day").set_emoji("◀️").add_to_container()
-    sub_buttons.add_button(ButtonStyle.DANGER, "-h").set_label("-1 hour").set_emoji("◀️").add_to_container()
-    sub_buttons.add_button(ButtonStyle.DANGER, "-m").set_label("-30 minutes").set_emoji("◀️").add_to_container()
-    os.environ['TZ'] = 'US/Eastern'
-    time.tzset()
-    time_seconds = round(time.time())
-    time_seconds -= time_seconds % 1800
-    embed = Embed(title="Time builder", description=f"timestamp: {time_seconds}: <t:{time_seconds}:F>")
-    await ctx.respond("Use the select menu to adjust the time", embed=embed, components=[add_buttons, sub_buttons])
-    try:
-        async with AdminPlugin.bot.stream(InteractionCreateEvent, timeout=15).filter(
-                ('interaction.user.id', ctx.author.id)) as stream:
-            async for event in stream:
-                await event.interaction.create_initial_response(ResponseType.DEFERRED_MESSAGE_UPDATE)
-                key = event.interaction.custom_id
-                if key == "Done":
-                    embed = (Embed(title="Final times", description=f"timestamp: {time_seconds}: <t:{time_seconds}:F>")
-                             .add_field(f"Default: `<t:{time_seconds}>`", f"<t:{time_seconds}>")
-                             .add_field(f"Short Time: `<t:{time_seconds}:t>`", f"<t:{time_seconds}:t>")
-                             .add_field(f"Long Time: `<t:{time_seconds}:T>`", f"<t:{time_seconds}:t>")
-                             .add_field(f"Short Date: `<t:{time_seconds}:d>`", f"<t:{time_seconds}:d>")
-                             .add_field(f"Long Date: `<t:{time_seconds}:D>`", f"<t:{time_seconds}:D>")
-                             .add_field(f"Short Date/Time: `<t:{time_seconds}:f>`", f"<t:{time_seconds}:f>")
-                             .add_field(f"Long Date/Time: `<t:{time_seconds}:F>`", f"<t:{time_seconds}:F>")
-                             .add_field(f"Relative Time: `<t:{time_seconds}:R>`", f"<t:{time_seconds}:R>")
-                             )
-                    await ctx.edit_last_response(f"<t:{time_seconds}", embed=embed, components=[])
-                    return
-                if key == "+w":
-                    time_seconds += 604800
-                    embed = Embed(title="Time builder", description=f"timestamp: {time_seconds}: <t:{time_seconds}:F>")
-                    await ctx.edit_last_response("Use the select menu to adjust the time", embed=embed,
-                                                 components=[add_buttons, sub_buttons])
-                elif key == "+d":
-                    time_seconds += 86400
-                    embed = Embed(title="Time builder", description=f"timestamp: {time_seconds}: <t:{time_seconds}:F>")
-                    await ctx.edit_last_response("Use the select menu to adjust the time", embed=embed,
-                                                 components=[add_buttons, sub_buttons])
-                elif key == "+h":
-                    time_seconds += 3600
-                    embed = Embed(title="Time builder", description=f"timestamp: {time_seconds}: <t:{time_seconds}:F>")
-                    await ctx.edit_last_response("Use the select menu to adjust the time", embed=embed,
-                                                 components=[add_buttons, sub_buttons])
-                elif key == "+m":
-                    time_seconds += 1800
-                    embed = Embed(title="Time builder", description=f"timestamp: {time_seconds}: <t:{time_seconds}:F>")
-                    await ctx.edit_last_response("Use the select menu to adjust the time", embed=embed,
-                                                 components=[add_buttons, sub_buttons])
-                elif key == "-w":
-                    time_seconds -= 604800
-                    embed = Embed(title="Time builder", description=f"timestamp: {time_seconds}: <t:{time_seconds}:F>")
-                    await ctx.edit_last_response("Use the select menu to adjust the time", embed=embed,
-                                                 components=[add_buttons, sub_buttons])
-                elif key == "-d":
-                    time_seconds -= 86400
-                    embed = Embed(title="Time builder", description=f"timestamp: {time_seconds}: <t:{time_seconds}:F>")
-                    await ctx.edit_last_response("Use the select menu to adjust the time", embed=embed,
-                                                 components=[add_buttons, sub_buttons])
-                elif key == "-h":
-                    time_seconds -= 3600
-                    embed = Embed(title="Time builder", description=f"timestamp: {time_seconds}: <t:{time_seconds}:F>")
-                    await ctx.edit_last_response("Use the select menu to adjust the time", embed=embed,
-                                                 components=[add_buttons, sub_buttons])
-                elif key == "-m":
-                    time_seconds -= 1800
-                    embed = Embed(title="Time builder", description=f"timestamp: {time_seconds}: <t:{time_seconds}:F>")
-                    await ctx.edit_last_response("Use the select menu to adjust the time", embed=embed,
-                                                 components=[add_buttons, sub_buttons])
-    except asyncio.TimeoutError:
-        await ctx.edit_last_response("Waited for 15 seconds... Timeout.", embed=None, components=[])
 
 
 @AdminPlugin.command()
