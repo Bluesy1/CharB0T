edb1a48cd8549f1346f4e72c3f2b0a81eb2dbd61
Bluesy <68259537+Bluesy1@users.noreply.github.com>
2022-05-06 07:38:47 -0700

    fix ... give_game_points ... i think??

--- charbot/main.py
+++ charbot/main.py
@@ -151,30 +151,31 @@ class CBot(commands.Bot):
                 )
                 await conn.execute("INSERT INTO bids (id, bid) VALUES ($1, 0)", user_id)
                 return points + bonus
+        elif user["particip_dt"] < __TIME__():
+            async with self.pool.acquire() as conn:
+                await conn.execute(
+                    "UPDATE daily_points SET last_particip_dt = $1, particip = $2, won = $3 WHERE id = $4",
+                    __TIME__(),
+                    points,
+                    bonus,
+                    user_id,
+                )
+                await conn.execute("UPDATE users SET points = points + $1 WHERE id = $2", points + bonus, user_id)
+        elif user["particip_dt"] == __TIME__():
+            if user["particip"] + points > 10:
+                real_points = 10 - user["particip"]
+                bonus = -(-(real_points * bonus) // points)
+                points = real_points
+            async with self.pool.acquire() as conn:
+                await conn.execute(
+                    "UPDATE daily_points SET particip = particip + $1, won = won + $2 WHERE id = $3",
+                    points,
+                    bonus,
+                    user_id,
+                )
+                await conn.execute("UPDATE users SET points = points + $1 WHERE id = $2", points + bonus, user_id)
         else:
-            if user["particip_dt"] < __TIME__():
-                async with self.pool.acquire() as conn:
-                    await conn.execute(
-                        "UPDATE daily_points SET last_particip_dt = $1, particip = $2, won = $3 WHERE id = $4",
-                        __TIME__(),
-                        points,
-                        bonus,
-                        user_id,
-                    )
-                    await conn.execute("UPDATE users SET points = points + $1 WHERE id = $2", points + bonus, user_id)
-            if user["particip_dt"] == __TIME__():
-                if user["particip"] + points > 10:
-                    real_points = 10 - user["particip"]
-                    bonus = -(-(real_points * bonus) // points)
-                    points = real_points
-                async with self.pool.acquire() as conn:
-                    await conn.execute(
-                        "UPDATE daily_points SET particip = particip + $1, won = won + $2 WHERE id = $3",
-                        points,
-                        bonus,
-                        user_id,
-                    )
-                    await conn.execute("UPDATE users SET points = points + $1 WHERE id = $2", points + bonus, user_id)
+            return 0
         return points + bonus
 
 
