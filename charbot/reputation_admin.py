ec2c4c4eba27131a77c0887481e85bd8497be870
Bluesy <68259537+Bluesy1@users.noreply.github.com>
2022-05-09 09:39:06 -0700

    fix a join

--- charbot/reputation_admin.py
+++ charbot/reputation_admin.py
@@ -545,7 +545,7 @@ class ReputationAdmin(
                 await interaction.followup.send(
                     f"Pool `{pool}`: {_pool['level']} level pool with {_pool['start']} base rep, {_pool['current']}"
                     f" current rep, {_pool['cap']} completed rep, and {_pool['reward']} reward. "
-                    f"and allowed for roles: <@{'>, <@&'.join(_pool['required_roles'])}>",
+                    f"and allowed for roles: <@{'>, <@&'.join(str(role) for role in _pool['required_roles'])}>",
                     file=image,
                 )
 
