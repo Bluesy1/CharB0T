643b855d519986d8258fe2604fb83cdb220b0f79
Bluesy <68259537+Bluesy1@users.noreply.github.com>
2022-05-10 18:10:19 -0700

    Revert "remove unfinished code"
    This reverts commit b468b87fd489af8712747f2fbb381cf6344298e2.

---
+++ charbot/stocks.py
@@ -0,0 +1,500 @@
+# -*- coding: utf-8 -*-
+#  ----------------------------------------------------------------------------
+#  MIT License
+#
+# Copyright (c) 2022 Bluesy
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#  ----------------------------------------------------------------------------
+"""Stock market game."""
+from dataclasses import dataclass
+from datetime import time
+from decimal import ROUND_HALF_UP, Context, Decimal
+from enum import Enum
+
+import discord
+import yfinance as yf
+from discord.ext import commands, tasks
+
+from main import CBot
+
+
+def fifteen_min_interval_generator():
+    """Generate 15-min intervals for a day from 00:00 to 23:45 as time objects."""
+    for hour in range(24):
+        for minute in range(0, 60, 15):
+            yield time(hour, minute)
+
+
+class tickers(Enum):
+    """Enum of tickers to be used in the stock commands."""
+
+    AAPL = "Apple Inc."
+    MSFT = "Microsoft Corporation"
+    GOOG = "Alphabet Inc."
+    FB = "Facebook Inc."
+    AMZN = "Amazon.com Inc."
+    TSLA = "Tesla Inc."
+    INTC = "Intel Corporation"
+    CSCO = "Cisco Systems Inc."
+    NVDA = "NVIDIA Corporation"
+    ORCL = "Oracle Corporation"
+    PYPL = "PayPal Holdings Inc."
+    QCOM = "Qualcomm Inc."
+    CMCSA = "Comcast Corporation"
+    GPRO = "GoPro Inc."
+    TWTR = "Twitter Inc."
+    GME = "GameStop Corp."
+    BTC_USD = "Bitcoin"
+    ETH_USD = "Ethereum"
+    LTC_USD = "Litecoin"
+    XRP_USD = "Ripple"
+    BCH_USD = "Bitcoin Cash"
+    ADA_USD = "Cardano"
+    XMR_USD = "Monero"
+    DASH_USD = "Dash"
+    EOS_USD = "EOS"
+
+
+@dataclass
+class Stock:
+    """Represents a stock.
+
+    Attributes
+    ----------
+    name : str
+        The name of the stock.
+    price : Decimal
+        The current price of the stock.
+    change : Decimal
+        The change in price of the stock.
+    change_percent : Decimal
+        The change in price of the stock as a percentage.
+    volume : Decimal
+        The volume of the stock.
+    """
+
+    name: str
+    price: Decimal
+    change: Decimal
+    change_percent: Decimal
+    volume: Decimal
+
+    def __init__(self, **data):
+        self.name = data["name"]
+        self.price = Decimal(data["price"], Context(prec=2, rounding=ROUND_HALF_UP))
+        self.change = Decimal(data["change"], Context(prec=2, rounding=ROUND_HALF_UP))
+        self.change_percent = Decimal(data["change_percent"], Context(prec=2, rounding=ROUND_HALF_UP))
+        self.volume = Decimal(data["volume"], Context(prec=2, rounding=ROUND_HALF_UP))
+
+    def as_dict(self):
+        """Return a dict representation of the stock."""
+        return {
+            "name": self.name,
+            "price": str(self.price),
+            "change": str(self.change),
+            "volume": str(self.volume),
+        }
+
+
+STOCKS = [
+    "AAPL",
+    "MSFT",
+    "GOOG",
+    "FB",
+    "AMZN",
+    "TSLA",
+    "INTC",
+    "CSCO",
+    "NVDA",
+    "ORCL",
+    "PYPL",
+    "QCOM",
+    "CMCSA",
+    "GPRO",
+    "TWTR",
+    "GME",
+    "BTC-USD",
+    "ETH-USD",
+    "LTC-USD",
+    "XRP-USD",
+    "BCH-USD",
+    "ADA-USD",
+    "XMR-USD",
+    "DASH-USD",
+    "EOS-USD",
+]
+
+
+@dataclass
+class StockDict:
+    """A dictionary of stock objects, with the stock symbol as the key.
+
+    Has a variety of methods to update, create, and export the stock objects.
+
+    Parameters
+    ----------
+    **data : dict[str, Stock]
+        The dictionary of stock objects.
+
+    Attributes
+    ----------
+    AAPL : Stock
+        The Apple stock object.
+    MSFT : Stock
+        The Microsoft stock object.
+    GOOG : Stock
+        The Google stock object.
+    FB : Stock
+        The Facebook stock object.
+    AMZN : Stock
+        The Amazon stock object.
+    TSLA : Stock
+        The Tesla stock object.
+    INTC : Stock
+        The Intel stock object.
+    CSCO : Stock
+        The Cisco stock object.
+    NVDA : Stock
+        The Nvidia stock object.
+    ORCL : Stock
+        The Oracle stock object.
+    PYPL : Stock
+        The PayPal stock object.
+    QCOM : Stock
+        The Qualcomm stock object.
+    CMCSA : Stock
+        The Comcast stock object.
+    GPRO : Stock
+        The GoPro stock object.
+    TWTR : Stock
+        The Twitter stock object.
+    GME : Stock
+        The GameStop stock object.
+    BTC : Stock
+        The Bitcoin stock object.
+    ETH : Stock
+        The Ethereum stock object.
+    LTC : Stock
+        The Litecoin stock object.
+    XRP : Stock
+        The Ripple stock object.
+    BCH : Stock
+        The Bitcoin Cash stock object.
+    ADA : Stock
+        The Cardano stock object.
+    XMR : Stock
+        The Monero stock object.
+    DASH : Stock
+        The Dash stock object.
+    EOS : Stock
+        The EOS stock object.
+    """
+
+    AAPL: Stock
+    MSFT: Stock
+    GOOG: Stock
+    FB: Stock
+    AMZN: Stock
+    TSLA: Stock
+    INTC: Stock
+    CSCO: Stock
+    NVDA: Stock
+    ORCL: Stock
+    PYPL: Stock
+    QCOM: Stock
+    CMCSA: Stock
+    GPRO: Stock
+    TWTR: Stock
+    GME: Stock
+    BTC: Stock
+    ETH: Stock
+    LTC: Stock
+    XRP: Stock
+    BCH: Stock
+    ADA: Stock
+    XMR: Stock
+    DASH: Stock
+    EOS: Stock
+
+    def __init__(self, **data: Stock):
+        self.AAPL = data["AAPL"]
+        self.MSFT = data["MSFT"]
+        self.GOOG = data["GOOG"]
+        self.FB = data["FB"]
+        self.AMZN = data["AMZN"]
+        self.TSLA = data["TSLA"]
+        self.INTC = data["INTC"]
+        self.CSCO = data["CSCO"]
+        self.NVDA = data["NVDA"]
+        self.ORCL = data["ORCL"]
+        self.PYPL = data["PYPL"]
+        self.QCOM = data["QCOM"]
+        self.CMCSA = data["CMCSA"]
+        self.GPRO = data["GPRO"]
+        self.TWTR = data["TWTR"]
+        self.GME = data["GME"]
+        self.BTC = data["BTC"]
+        self.ETH = data["ETH"]
+        self.LTC = data["LTC"]
+        self.XRP = data["XRP"]
+        self.BCH = data["BCH"]
+        self.ADA = data["ADA"]
+        self.XMR = data["XMR"]
+        self.DASH = data["DASH"]
+        self.EOS = data["EOS"]
+
+    @classmethod
+    def empty(cls):
+        """Return an empty StockDict object."""
+        return cls(
+            AAPL=Stock(name="Apple Inc.", price=0, change=0, change_percent=0, volume=0),
+            MSFT=Stock(
+                name="Microsoft Corporation",
+                price=0,
+                change=0,
+                change_percent=0,
+                volume=0,
+            ),
+            GOOG=Stock(name="Alphabet Inc.", price=0, change=0, change_percent=0, volume=0),
+            FB=Stock(name="Facebook Inc.", price=0, change=0, change_percent=0, volume=0),
+            AMZN=Stock(name="Amazon.com Inc.", price=0, change=0, change_percent=0, volume=0),
+            TSLA=Stock(name="Tesla Inc.", price=0, change=0, change_percent=0, volume=0),
+            INTC=Stock(name="Intel Corporation", price=0, change=0, change_percent=0, volume=0),
+            CSCO=Stock(name="Cisco Systems Inc.", price=0, change=0, change_percent=0, volume=0),
+            NVDA=Stock(name="NVIDIA Corporation", price=0, change=0, change_percent=0, volume=0),
+            ORCL=Stock(name="Oracle Corporation", price=0, change=0, change_percent=0, volume=0),
+            PYPL=Stock(
+                name="PayPal Holdings Inc.",
+                price=0,
+                change=0,
+                change_percent=0,
+                volume=0,
+            ),
+            QCOM=Stock(name="Qualcomm Inc.", price=0, change=0, change_percent=0, volume=0),
+            CMCSA=Stock(
+                name="Comcast Corporation",
+                price=0,
+                change=0,
+                change_percent=0,
+                volume=0,
+            ),
+            GPRO=Stock(name="GoPro Inc.", price=0, change=0, change_percent=0, volume=0),
+            TWTR=Stock(name="Twitter Inc.", price=0, change=0, change_percent=0, volume=0),
+            GME=Stock(name="GameStop Corp.", price=0, change=0, change_percent=0, volume=0),
+            BTC=Stock(name="Bitcoin", price=0, change=0, change_percent=0, volume=0),
+            ETH=Stock(name="Ethereum", price=0, change=0, change_percent=0, volume=0),
+            LTC=Stock(name="Litecoin", price=0, change=0, change_percent=0, volume=0),
+            XRP=Stock(name="Ripple", price=0, change=0, change_percent=0, volume=0),
+            BCH=Stock(name="Bitcoin Cash", price=0, change=0, change_percent=0, volume=0),
+            ADA=Stock(name="Cardano", price=0, change=0, change_percent=0, volume=0),
+            XMR=Stock(name="Monero", price=0, change=0, change_percent=0, volume=0),
+            DASH=Stock(name="Dash", price=0, change=0, change_percent=0, volume=0),
+            EOS=Stock(name="EOS", price=0, change=0, change_percent=0, volume=0),
+        )
+
+    async def update(self, stock: tuple[str, Stock]):
+        """Update a stock object with the given stock."""
+        if any(
+            item.is_nan()
+            for item in [
+                stock[1].price,
+                stock[1].change,
+                stock[1].change_percent,
+                stock[1].volume,
+            ]
+        ):
+            return
+        if stock[0] == "AAPL":
+            self.AAPL = stock[1]
+        elif stock[0] == "MSFT":
+            self.MSFT = stock[1]
+        elif stock[0] == "GOOG":
+            self.GOOG = stock[1]
+        elif stock[0] == "FB":
+            self.FB = stock[1]
+        elif stock[0] == "AMZN":
+            self.AMZN = stock[1]
+        elif stock[0] == "TSLA":
+            self.TSLA = stock[1]
+        elif stock[0] == "INTC":
+            self.INTC = stock[1]
+        elif stock[0] == "CSCO":
+            self.CSCO = stock[1]
+        elif stock[0] == "NVDA":
+            self.NVDA = stock[1]
+        elif stock[0] == "ORCL":
+            self.ORCL = stock[1]
+        elif stock[0] == "PYPL":
+            self.PYPL = stock[1]
+        elif stock[0] == "QCOM":
+            self.QCOM = stock[1]
+        elif stock[0] == "CMCSA":
+            self.CMCSA = stock[1]
+        elif stock[0] == "GPRO":
+            self.GPRO = stock[1]
+        elif stock[0] == "TWTR":
+            self.TWTR = stock[1]
+        elif stock[0] == "GME":
+            self.GME = stock[1]
+        elif stock[0] == "BTC":
+            self.BTC = stock[1]
+        elif stock[0] == "ETH":
+            self.ETH = stock[1]
+        elif stock[0] == "LTC":
+            self.LTC = stock[1]
+        elif stock[0] == "XRP":
+            self.XRP = stock[1]
+        elif stock[0] == "BCH":
+            self.BCH = stock[1]
+        elif stock[0] == "ADA":
+            self.ADA = stock[1]
+        elif stock[0] == "XMR":
+            self.XMR = stock[1]
+        elif stock[0] == "DASH":
+            self.DASH = stock[1]
+        elif stock[0] == "EOS":
+            self.EOS = stock[1]
+
+    @classmethod
+    def from_dict(cls, data: dict[str, dict[str, str | Decimal]]):
+        """Initialize a StockDict object from a dict."""
+        return cls(**data)
+
+    async def export(self):
+        """Export the stock objects to a dictionary."""
+        return {
+            "AAPL": self.AAPL.as_dict(),
+            "MSFT": self.MSFT.as_dict(),
+            "GOOG": self.GOOG.as_dict(),
+            "FB": self.FB.as_dict(),
+            "AMZN": self.AMZN.as_dict(),
+            "TSLA": self.TSLA.as_dict(),
+            "INTC": self.INTC.as_dict(),
+            "CSCO": self.CSCO.as_dict(),
+            "NVDA": self.NVDA.as_dict(),
+            "ORCL": self.ORCL.as_dict(),
+            "PYPL": self.PYPL.as_dict(),
+            "QCOM": self.QCOM.as_dict(),
+            "CMCSA": self.CMCSA.as_dict(),
+            "GPRO": self.GPRO.as_dict(),
+            "TWTR": self.TWTR.as_dict(),
+            "GME": self.GME.as_dict(),
+            "BTC": self.BTC.as_dict(),
+            "ETH": self.ETH.as_dict(),
+            "LTC": self.LTC.as_dict(),
+            "XRP": self.XRP.as_dict(),
+            "BCH": self.BCH.as_dict(),
+            "ADA": self.ADA.as_dict(),
+            "XMR": self.XMR.as_dict(),
+            "DASH": self.DASH.as_dict(),
+            "EOS": self.EOS.as_dict(),
+        }
+
+
+def get_stock(symbol: str) -> tuple[str, Stock]:
+    """Get the stock data from the API.
+
+    Parameters
+    ----------
+    symbol : str
+        The stock symbol.
+
+    Returns
+    -------
+    tuple[str, Stock]
+        The stock symbol and the stock object.
+    """
+    data = yf.download(symbol, period="1d", interval="15m")
+    try:
+        return symbol, Stock(
+            **{
+                "name": tickers[symbol.replace("-", "_")].value,
+                "price": Decimal(str(data.iloc[-2]["Close"])),
+                "change": (Decimal(str(data.iloc[-2]["Close"])) - Decimal(str(data.iloc[-3]["Close"]))),
+                "change_percent": (
+                    100
+                    * (Decimal(str(data.iloc[-2]["Close"])) - Decimal(str(data.iloc[-3]["Close"])))
+                    / Decimal(str(data.iloc[-3]["Close"]))
+                ),
+                "volume": Decimal(str(data.iloc[-3]["Volume"])),
+            }
+        )
+    except KeyError:
+        return symbol, Stock(
+            **{
+                "name": "Unknown",
+                "price": Decimal("NaN"),
+                "change": Decimal("NaN"),
+                "change_percent": Decimal("NaN"),
+                "volume": Decimal("NaN"),
+            }
+        )
+
+
+class Stocks(commands.GroupCog, name="stocks", description="Stocks commands"):
+    """Stocks cog.
+
+    This cog contains commands for getting stock data.
+
+    Parameters
+    ----------
+    bot : CBot
+        The bot object.
+
+    Attributes
+    ----------
+    bot : CBot
+        The bot object.
+    """
+
+    def __init__(self, bot: CBot):
+        super().__init__()
+        self.bot = bot
+        self._stocks: StockDict = StockDict.empty()
+
+    async def get_stocks(self) -> dict[str, Stock]:
+        """Get _stocks."""
+        _tasks = [await self.bot.loop.run_in_executor(self.bot.executor, get_stock, symbol) for symbol in STOCKS]
+        return {result[0].split("-")[0]: result[1] for result in _tasks}
+
+    async def cog_load(self) -> None:
+        """Cog load callback."""
+        self._stocks = StockDict(**await self.get_stocks())
+        self.update_stocks.start()
+
+    async def cog_unload(self) -> None:  # skipcq: PYL-W0236
+        """Cog unload callback."""
+        self.update_stocks.cancel()
+
+    @tasks.loop(time=list(fifteen_min_interval_generator()))  # skipcq: PYL-E1123
+    async def update_stocks(self) -> None:
+        """Update _stocks."""
+        self._stocks = StockDict(**await self.get_stocks())
+
+
+async def setup(bot: CBot):
+    """Load the cog.
+
+    Parameters
+    ----------
+    bot : CBot
+        The bot object
+    """
+    await bot.add_cog(Stocks(bot), override=True, guild=discord.Object(id=225345178955808768))
