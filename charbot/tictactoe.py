511c4270e6e764ea28b67a6defdc17618e631757
Bluesy <68259537+Bluesy1@users.noreply.github.com>
2022-05-10 18:10:19 -0700

    Revert "break apart next computer move"
    This reverts commit 4efd315bec92bf9f7996b67914d665845d9d7175.

--- charbot/tictactoe.py
+++ charbot/tictactoe.py
@@ -525,80 +525,32 @@ class TicTacHard(TicTacEasy):
         assert isinstance(move_y, list)  # skipcq: BAN-B101
         return move_x[0], move_y[0]
 
-    def _available_moves(self) -> list[tuple[int, int]]:
-        """Return a list of available moves.
+    def _next_move_easy(self) -> tuple[int, int] | tuple[list[int], list[int]]:
+        """Make a move, and return the cell that was played.
 
         Returns
         -------
-        list[tuple[int, int]]
-            A list of available moves.
+        tuple[int, int] | tuple[list[int], list[int]]
+            The cell that was played.
         """
-        return [(i, j) for i in range(0, self.dim_sz) for j in range(0, self.dim_sz) if self.board[i][j] == "blur"]
-
-    def _win_spots(self, pick1: str, pick2: str) -> tuple[list[tuple[int, int]], list[tuple[int, int]]]:
-        """Return a list of winning moves.
-
-        Parameters
-        ----------
-        pick1 : str
-            The pick to check for the player.
-        pick2 : str
-            The pick to check for the computer.
-
-        Returns
-        -------
-        list[tuple[int, int]]
-            A list of winning moves.
-        """
-        win_spots1 = []
-        win_spots2 = []
+        available_moves = []  # will carry all available moves
+        player_win_spot = []  # if player (user Wins)
+        comp_pick = "O"
+        if self.pick == "O":
+            comp_pick = "X"
         for i in range(0, self.dim_sz):
             for j in range(0, self.dim_sz):
-                if self.board[i][j] == "blur":
-                    self.board[i][j] = pick1
-                    if self.check_win() == 1:
-                        win_spots1.append((i, j))
-                    self.board[i][j] = pick2
-                    if self.check_win() == 0:
-                        win_spots2.append((i, j))
+                if self.board[i][j] == "blur":  # BLANK
+                    t = (i, j)
+                    available_moves.append(t)  # add it to available moves
+                    self.board[i][j] = comp_pick  # Check if I (Computer can win)
+                    if self.check_win() == 0:  # Best Case I(Computer) win!
+                        return i, j
+                    self.board[i][j] = self.pick
+                    if self.check_win() == 1:  # Second Best Case, he (player) didn't won
+                        player_win_spot.append(t)
                     self.board[i][j] = "blur"
-        return win_spots1, win_spots2
 
-    def _simple_move_checks(
-        self,
-        comp: list[tuple[int, int]],
-        player: list[tuple[int, int]],
-        available: list[tuple[int, int]],
-        comp_pick: str,
-    ) -> int | tuple[int, int]:
-        """Check if there is a simple move.
-
-        Parameters
-        ----------
-        comp : list[tuple[int, int]]
-            The computer's moves that would win the game for the computer.
-        player : list[tuple[int, int]]
-            The player's moves that would win the game for the player.
-        available : list[tuple[int, int]]
-            The available moves.
-        comp_pick : str
-            The pick for the computer.
-
-        Returns
-        -------
-        int | tuple[int, int]
-            The move to make, or 0 if there is no simple move.
-        """
-        if len(comp) != 0:
-            return comp[0]
-        if len(player) != 0:
-            self.board[player[0][0]][player[0][1]] = comp_pick
-            return player[0][0], player[0][1]
-        if len(available) == 1:
-            self.board[available[0][0]][available[0][1]] = comp_pick
-            return available[0][0], available[0][1]
-        if len(available) == 0:
-            return -1, -1
         if self.board[0][0] == self.pick and self.board[2][2] == self.pick:
             if self.board[0][2] == "blur":
                 self.board[0][2] = comp_pick
@@ -613,88 +565,57 @@ class TicTacHard(TicTacEasy):
             if self.board[2][2] == "blur":
                 self.board[2][2] = comp_pick
                 return 2, 2
-        return 0
+        if len(player_win_spot) != 0:
+            self.board[player_win_spot[0][0]][player_win_spot[0][1]] = comp_pick
+            return player_win_spot[0][0], player_win_spot[0][1]
+        if len(available_moves) == 1:
+            self.board[available_moves[0][0]][available_moves[0][1]] = comp_pick
+            return [available_moves[0][0]], [available_moves[0][1]]
+        if len(available_moves) == 0:
+            return -1, -1
 
-    def _complex_move_checks(self, available: list[tuple[int, int]], comp_pick: str) -> int | tuple[int, int]:
-        """Check if there is a complex move.
-
-        Parameters
-        ----------
-        available : list[tuple[int, int]]
-            The available moves.
-        comp_pick : str
-            The pick for the computer.
-
-        Returns
-        -------
-        int | tuple[int, int]
-            The move to make, or 0 if there is no complex move.
-        """
         c1, c2 = self.dim_sz // 2, self.dim_sz // 2
+        if (c1, c2) in available_moves:  # CENTER
+            self.board[c1][c2] = comp_pick
+            return c1, c2
         for i in range(c1 - 1, -1, -1):  # IN TO OUT
             gap = c1 - i
             # checking  - 4 possibilities at max
             # EDGES
-            if (c1 - gap, c2 - gap) in available:
+            if (c1 - gap, c2 - gap) in available_moves:
                 self.board[c1 - gap][c2 - gap] = comp_pick
                 return c1 - gap, c2 - gap
-            if (c1 - gap, c2 + gap) in available:
+            if (c1 - gap, c2 + gap) in available_moves:
                 self.board[c1 - gap][c2 + gap] = comp_pick
                 return c1 - gap, c2 + gap
-            if (c1 + gap, c2 - gap) in available:
+            if (c1 + gap, c2 - gap) in available_moves:
                 self.board[c1 + gap][c2 - gap] = comp_pick
                 return c1 + gap, c2 - gap
-            if (c1 + gap, c2 + gap) in available:
+            if (c1 + gap, c2 + gap) in available_moves:
                 self.board[c1 + gap][c2 + gap] = comp_pick
                 return c1 + gap, c2 + gap
 
             # Four Lines
 
             for j in range(0, gap):
-                if (c1 - gap, c2 - gap + j) in available:  # TOP LEFT TO TOP RIGHT
+                if (c1 - gap, c2 - gap + j) in available_moves:  # TOP LEFT TO TOP RIGHT
                     self.board[c1 - gap][c2 - gap + j] = comp_pick
                     return c1 - gap, c2 - gap + j
                 if (
                     c1 + gap,
                     c2 - gap + j,
-                ) in available:  # BOTTOM LEFT TO BOTTOM RIGHT
+                ) in available_moves:  # BOTTOM LEFT TO BOTTOM RIGHT
                     self.board[c1 + gap][c2 - gap + j] = comp_pick
                     return c1 + gap, c2 - gap + j
-                if (c1 - gap, c2 - gap) in available:  # LEFT TOP TO LEFT BOTTOM
+                if (c1 - gap, c2 - gap) in available_moves:  # LEFT TOP TO LEFT BOTTOM
                     self.board[c1 - gap + j][c2 - gap] = comp_pick
                     return c1 - gap + j, c2 - gap
                 if (
                     c1 - gap + j,
                     c2 + gap,
-                ) in available:  # RIGHT TOP TO RIGHT BOTTOM
+                ) in available_moves:  # RIGHT TOP TO RIGHT BOTTOM
                     self.board[c1 - gap + j][c2 + gap] = comp_pick
                     return c1 - gap + j, c2 + gap
-        return 0
-
-    def _next_move_easy(self) -> tuple[int, int] | tuple[list[int], list[int]]:
-        """Make a move, and return the cell that was played.
-
-        Returns
-        -------
-        tuple[int, int] | tuple[list[int], list[int]]
-            The cell that was played.
-        """
-        available_moves = self._available_moves()  # will carry all available moves
-        comp_pick = "O"
-        if self.pick == "O":
-            comp_pick = "X"
-        # if player (user Wins), if computer (computer Wins)
-        player_win_spot, comp_win_spots = self._win_spots(self.pick, comp_pick)
-        simple_move = self._simple_move_checks(comp_win_spots, player_win_spot, available_moves, comp_pick)
-        if isinstance(simple_move, tuple):
-            return simple_move
-        c1, c2 = self.dim_sz // 2, self.dim_sz // 2
-        if (c1, c2) in available_moves:  # CENTER
-            self.board[c1][c2] = comp_pick
-            return c1, c2
-        complex_move = self._complex_move_checks(available_moves, comp_pick)
-        if isinstance(complex_move, tuple):
-            return complex_move
         raise RuntimeError("No moves available")
 
 
