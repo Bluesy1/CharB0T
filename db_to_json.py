a30705566c116cad9de62e369651b2e7ebb24458
Bluesy <68259537+Bluesy1@users.noreply.github.com>
2022-06-02 16:58:33 -0700

    Test github action

---
+++ db_to_json.py
@@ -0,0 +1,40 @@
+import psycopg2
+import json
+from os import getenv, environ
+
+
+def _(env: str) -> str | None:
+    print(getenv(f"{env.upper()}"))
+    return getenv(f"{env.upper()}")
+
+
+conn = psycopg2.connect(database=_("db"), user=_("db_user"), password=_("pass"), host=_("host"), port=_("port"))
+
+
+def avatar(avatar_hash: str | None, user_id: int, discrim: int) -> str:
+    if avatar_hash is None:
+        return f"https://cdn.discordapp.com/embed/avatars/{discrim % 5}.png"
+    return f"https://cdn.discordapp.com/avatars/{user_id}/{avatar_hash}.png"
+
+
+try:
+    with conn, conn.cursor() as cur:
+        cur.execute("SELECT * FROM xp_users ORDER BY xp DESC")
+        environ["LEADERBOARD_JSON"] = json.dumps(
+            [
+                {
+                    "id": user[0],
+                    "name":user[1],
+                    "discrim": user[2],
+                    "xp": user[3],
+                    "detailed_xp": user[4],
+                    "level": user[5],
+                    "messages": user[6],
+                    "avatar": avatar(user[7], user[0], int(user[2])),
+                }
+                for user in cur.fetchall()
+            ],
+        )
+except Exception as e:
+    with open("_data/users.json", "r") as f:
+        environ["LEADERBOARD_JSON"] = json.dumps(json.load(f))
