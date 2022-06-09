dcee99f761baa1c700b2f45bafaf27a8b15c702b
Bluesy <68259537+Bluesy1@users.noreply.github.com>
2022-06-08 19:27:09 -0700

    update leaderboard CI to use orjson and have a backup check to not commit

--- db_to_json.py
+++ db_to_json.py
@@ -1,10 +1,9 @@
 import psycopg2
-import json
-from os import getenv, environ
+import orjson
+from os import getenv
+from typing import Callable
 
-
-def _(env: str) -> str | None:
-    return getenv(f"{env.upper()}")
+_: Callable[[str], str | None] = lambda x: getenv(f"{x.upper()}")
 
 
 conn = psycopg2.connect(database=_("db"), user=_("db_user"), password=_("pass"), host=_("host"), port=_("port"))
@@ -15,35 +14,41 @@ def avatar(avatar_hash: str | None, user_id: int, discriminator: int) -> str:
         return f"https://cdn.discordapp.com/embed/avatars/{discriminator % 5}.png"
     return f"https://cdn.discordapp.com/avatars/{user_id}/{avatar_hash}.png"
 
+
 def get_str(xp):
     if xp < 1000:
         return str(xp)
-    if xp >= 1000 and xp < 1000000:
+    if 1000 <= xp < 1000000:
         return str(round(xp / 1000, 1)) + "k"
     if xp > 1000000:
         return str(round(xp / 1000000, 1)) + "M"
 
+
 with conn, conn.cursor() as cur:
     cur.execute("SELECT *, ROW_NUMBER() OVER(ORDER BY xp DESC) FROM xp_users ORDER BY xp DESC")
-    with open("_data/users.json", "w") as f:
-        json.dump(
-            [
-                {
-                    "id": user[0],
-                    "name": str(user[1]).encode("utf-8", "ignore").decode("utf-8", "ignore"),
-                    "discriminator": user[2],
-                    "xp": get_str(int(user[3])),
-                    "detailed_xp": user[4],
-                    "level": user[5],
-                    "messages": user[6],
-                    "avatar": avatar(user[7], user[0], int(user[2])),
-                    "gang": user[8],
-                    "prestige": user[9],
-                    "rank": user[10],
-                }
-                for user in cur.fetchall()
-            ],
-            f,
-            indent=4,
-            ensure_ascii=False,
-        )
+    with open("_data/users.json", "b") as r:
+        old = r.read()
+    with open("_data/users.json", "wb") as f:
+        new = orjson.dumps(
+                [
+                    {
+                        "id": str(user[0]),
+                        "name": user[1],
+                        "discriminator": user[2],
+                        "xp": user[3],
+                        "detailed_xp": user[4],
+                        "level": user[5],
+                        "messages": user[6],
+                        "avatar": avatar(user[7], user[0], int(user[2])),
+                        "gang": user[8],
+                        "prestige": user[9],
+                        "rank": user[10],
+                    }
+                    for user in cur.fetchall()
+                ],
+                option=orjson.OPT_INDENT_2,
+            )
+        if old != new:
+            f.write(new)
+        with open(_("github_env"), "a") as envfile:
+            envfile.write(f"DO_COMMIT={'true' if old != new else 'false'}")
