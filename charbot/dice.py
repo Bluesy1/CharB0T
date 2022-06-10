c6bc9ca47e1aa110724de282d7d835631c775c31
Bluesy <68259537+Bluesy1@users.noreply.github.com>
2022-06-09 18:10:54 -0700

    isolate code in preparation to make tests

--- charbot/dice.py
+++ charbot/dice.py
@@ -23,15 +23,61 @@
 # SOFTWARE.
 #  ----------------------------------------------------------------------------
 """Rolls dice."""
-import sys
+import random
 
 import discord
 from discord.ext import commands
 from discord.ext.commands import Cog, Context
 
 
-sys.path.append("..")
-from helpers import roller  # skipcq: FLK-E402  # noqa: E402
+def roll(arg: str) -> str:
+    """Dice roller.
+
+    Parameters
+    ----------
+    arg : str
+        Dice roll string
+    """
+    roll_error = (
+        "Error invalid argument: specified dice can only be d<int>,"
+        " or if a constant modifier must be a "
+        "perfect integer, positive or negative, "
+        "connected with `+`, and no spaces."
+    )
+    if "+" in arg:
+        dice = arg.split("+")
+    else:
+        dice = [str(arg)]
+    try:
+        sums = 0
+        rolls = []
+        for die in dice:
+            if "d" in die:
+                try:
+                    num_rolls = int(die[: die.find("d")])
+                except ValueError:
+                    num_rolls = 1
+                i = 1
+                while i <= num_rolls:
+                    # fmt: off
+                    roll1 = random.randint(1, int(die[die.find("d") + 1:]))
+                    # fmt: on
+                    rolls.append(roll1)
+                    sums += roll1
+                    i += 1
+            else:
+                try:
+                    rolls.append(int(die))
+                    sums += int(die)
+                except ValueError:
+                    return roll_error
+        output = "`"
+        for roll1 in rolls:
+            output += str(roll1) + ", "
+        output = output[:-2]
+        return f"rolled `{arg}` got {output}` for a total value of: {str(sums)}"
+    except Exception:  # skipcq: PYL-W0703
+        return roll_error
 
 
 class Roll(Cog):
@@ -84,7 +130,7 @@ class Roll(Cog):
         dice : str
             The dice to roll.
         """
-        await ctx.reply(f"{ctx.author.mention} {roller.roll(dice)}", mention_author=True)
+        await ctx.reply(f"{ctx.author.mention} {roll(dice)}", mention_author=True)
 
 
 async def setup(bot: commands.Bot):
