0d87e8f7ab626a653698618b72e93f6820b8ae7a
Bluesy <68259537+Bluesy1@users.noreply.github.com>
2022-07-11 18:48:40 -0700

    complete testing of config and log filter class

--- tests/charbot/test_dunders.py
+++ tests/charbot/test_dunders.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 import builtins
+import logging
 from io import BytesIO
 
+import pytest
+
+import charbot
 from charbot import Config
 
 
-def test_config(monkeypatch):
+def test_config(monkeypatch, caplog):
     """Test config."""
 
     class mock_open:
@@ -21,6 +25,81 @@ def test_config(monkeypatch):
         def __exit__(self, exc_type, exc_val, exc_tb):  # skipcq
             pass
 
+    caplog.set_level(logging.DEBUG)
     monkeypatch.setattr(builtins, "open", mock_open)
     assert Config["calendar"] == {"key": "key"}
+    caplog.clear()
     assert Config("calendar", "key") == "key"
+    log = caplog.record_tuples[0]
+    assert log[0] == "charbot.config"
+    assert log[1] == logging.INFO
+    assert log[2] == "Got key calendar:key from config file."
+
+    with pytest.raises(KeyError):
+        caplog.clear()
+        Config("calendar", "badkey")
+    log = caplog.record_tuples[0]
+    assert log[0] == "charbot.config"
+    assert log[1] == logging.ERROR
+    assert log[2] == "Tried to get key calendar:badkey from config file, but it was not found."
+
+    class Test(object):
+        def __str__(self):
+            return "Test"
+
+    obj = Test()
+    with pytest.raises(TypeError):
+        caplog.clear()
+        Config("calendar", obj)
+    log = caplog.record_tuples[0]
+    assert log[0] == "charbot.config"
+    assert log[1] == logging.ERROR
+    assert (
+        log[2] == f"Tried to get key calendar:Test from config file, but a non string key {obj!r} of type "
+        f"{type(obj)} was passed."
+    )
+
+
+@pytest.mark.parametrize(
+    ("name", "level", "text", "expected", "last", "expected_last"),
+    [
+        ("Info_Test_Non_Presence", logging.INFO, "Test", True, True, False),
+        ("Info_Test_Presence", logging.INFO, "presence_update", True, False, False),
+        ("Debug_Test_Non_Presence", logging.DEBUG, "Test", True, True, False),
+        (
+            "Debug_Test_Presence_1",
+            logging.DEBUG,
+            "[2022-07-12 01:16:13] [DEBUG   ] discord.gateway: For Shard ID None: WebSocket Event: "
+            "{'t': 'PRESENCE_UPDATE', 's': 346863, 'op': 0, 'd': {'user': {'id': '147223093721563136'}, "
+            "'status': 'online', 'guild_id': '225345178955808768', 'client_status': "
+            "{'mobile': 'online', 'desktop': 'idle'}, 'activities': []}}",
+            False,
+            False,
+            True,
+        ),
+        (
+            "Debug_Test_Presence_2",
+            logging.DEBUG,
+            "[2022-07-12 01:16:13] [DEBUG   ] discord.client: Dispatching event socket_event_type",
+            False,
+            True,
+            False,
+        ),
+        (
+            "Debug_Test_Presence_3",
+            logging.DEBUG,
+            "[2022-07-12 01:16:13] [DEBUG   ] discord.client: Dispatching event presence_update",
+            False,
+            False,
+            False,
+        ),
+    ],
+)
+def test_filter(name, level, text, expected, last, expected_last):
+    """Test filter."""
+
+    log_filter = charbot.PresenceFilter()
+    log_filter.last_presence = last
+    test_record = logging.LogRecord(name, level, __file__, 67, text, (), None)
+    assert log_filter.filter(test_record) is expected
+    assert log_filter.last_presence is expected_last
