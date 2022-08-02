b42f4f342d1110263134e5ce85c761cc74e23b9b
Bluesy <68259537+Bluesy1@users.noreply.github.com>
2022-08-02 16:47:08 -0700

    fix dice.py tests

--- tests/charbot/test_dice.py
+++ tests/charbot/test_dice.py
@@ -4,6 +4,7 @@ import random
 import discord
 import pytest
 from discord.ext import commands
+from fluent.runtime import FluentResourceLoader, FluentLocalization
 from pytest_mock import MockerFixture
 
 from charbot import dice
@@ -15,39 +16,53 @@ def not_random(monkeypatch):
     monkeypatch.setattr(random, "randint", lambda *args: 1)
 
 
-def test_valid_roll():
+@pytest.fixture()
+def user_localizer() -> tuple[str, FluentLocalization]:
+    """Return a user localizer.
+
+    Returns:
+        tuple[str, FluentLocalization]: A tuple of the user's locale and the user's localizer.
+    """
+    return "User", FluentLocalization(["en-US"], ["dice.ftl"], FluentResourceLoader("i18n/{locale}"))
+
+
+def test_valid_roll(user_localizer):
     """Test valid roll."""
-    assert dice.roll("1d4+5") == "rolled `1d4+5` got `1, 5` for a total value of: 6"
+    assert dice.roll("1d4+5", *user_localizer) == "User rolled `1d4+5` and got `1, 5`for a total of `6`."
 
 
-def test_valid_implicit_roll():
+def test_valid_implicit_roll(user_localizer):
     """Test valid implicit roll."""
-    assert dice.roll("d4+5") == "rolled `d4+5` got `1, 5` for a total value of: 6"
+    assert dice.roll("d4+5", *user_localizer) == "User rolled `d4+5` and got `1, 5`for a total of `6`."
 
 
-def test_valid_implicit_roll_no_bonus():
+def test_valid_implicit_roll_no_bonus(user_localizer):
     """Test valid implicit roll without bonus."""
-    assert dice.roll("d4") == "rolled `d4` got `1` for a total value of: 1"
+    assert dice.roll("d4", *user_localizer) == "User rolled `d4` and got `1`for a total of `1`."
 
 
-def test_valid_implicit_roll_only_bonus():
+def test_valid_implicit_roll_only_bonus(user_localizer):
     """Test valid implicit roll only bonus."""
-    assert dice.roll("5") == "rolled `5` got `5` for a total value of: 5"
+    assert dice.roll("5", *user_localizer) == "User rolled `5` and got `5`for a total of `5`."
 
 
-def test_invalid_roll():
+def test_invalid_roll(user_localizer):
     """Test invalid roll."""
-    assert dice.roll("1e4+5") == (
-        "Error invalid argument: specified dice can only be d<int>, or if a constant modifier must be a perfect"
-        " integer, positive or negative, connected with `+`, and no spaces."
+    assert dice.roll("1e4+5", *user_localizer) == (
+        "User:\n"
+        "Error invalid argument:\n"
+        " Specified dice can only be d<int>, or if a constant modifier must be a "
+        "perfect integer, positive or negative, connected with `+`, and no spaces."
     )
 
 
-def test_invalid_die():
+def test_invalid_die(user_localizer):
     """Test invalid die."""
-    assert dice.roll("1de+5") == (
-        "Error invalid argument: specified dice can only be d<int>, or if a constant modifier must be a perfect"
-        " integer, positive or negative, connected with `+`, and no spaces."
+    assert dice.roll("1de+5", *user_localizer) == (
+        "User:\n"
+        "Error invalid argument:\n"
+        " Specified dice can only be d<int>, or if a constant modifier must be a "
+        "perfect integer, positive or negative, connected with `+`, and no spaces."
     )
 
 
@@ -56,12 +71,12 @@ async def test_valid_roll_async(mocker: MockerFixture):
     """Test valid roll command."""
     mock_ctx = mocker.Mock(spec=commands.Context)
     mock_bot = mocker.Mock(spec=commands.Bot)
+    mock_bot.localizer_loader = FluentResourceLoader("i18n/{locale}")
+    mock_ctx.bot = mock_bot
     mock_ctx.author.mention = "mock"
     cog = dice.Roll(mock_bot)
     await cog.roll.__call__(mock_ctx, mock_ctx, dice="1d4+5")  # type: ignore  # skipcq: PYL-E1102
-    mock_ctx.reply.assert_called_once_with(
-        "mock rolled `1d4+5` got `1, 5` for a total value of: 6", mention_author=True
-    )
+    mock_ctx.reply.assert_called_once_with("mock rolled `1d4+5` and got `1, 5`for a total of `6`.", mention_author=True)
 
 
 @pytest.mark.asyncio
@@ -69,12 +84,16 @@ async def test_invalid_roll_async(mocker: MockerFixture):
     """Test invalid roll command."""
     mock_ctx = mocker.Mock(spec=commands.Context)
     mock_bot = mocker.Mock(spec=commands.Bot)
+    mock_bot.localizer_loader = FluentResourceLoader("i18n/{locale}")
+    mock_ctx.bot = mock_bot
     mock_ctx.author.mention = "mock"
     cog = dice.Roll(mock_bot)
     await cog.roll.__call__(mock_ctx, mock_ctx, dice="1e4+5")  # type: ignore  # skipcq: PYL-E1102
     mock_ctx.reply.assert_called_once_with(
-        "mock Error invalid argument: specified dice can only be d<int>, or if a constant"
-        " modifier must be a perfect integer, positive or negative, connected with `+`, and no spaces.",
+        "mock:\n"
+        "Error invalid argument:\n"
+        " Specified dice can only be d<int>, or if a constant modifier must be a "
+        "perfect integer, positive or negative, connected with `+`, and no spaces.",
         mention_author=True,
     )
 
@@ -84,6 +103,8 @@ def test_cog_check_no_guild(mocker: MockerFixture):
     mock_ctx = mocker.Mock(spec=commands.Context)
     mock_ctx.guild = None
     mock_bot = mocker.Mock(spec=commands.Bot)
+    mock_bot.localizer_loader = FluentResourceLoader("i18n/{locale}")
+    mock_ctx.bot = mock_bot
     cog = dice.Roll(mock_bot)
     assert cog.cog_check(mock_ctx) is False
 
@@ -97,6 +118,8 @@ def test_cog_check_not_allowed(mocker: MockerFixture):
     mock_role.id = 0
     mock_ctx.author.roles = [mock_role]
     mock_bot = mocker.Mock(spec=commands.Bot)
+    mock_bot.localizer_loader = FluentResourceLoader("i18n/{locale}")
+    mock_ctx.bot = mock_bot
     cog = dice.Roll(mock_bot)
     assert cog.cog_check(mock_ctx) is False
 
@@ -110,6 +133,8 @@ def test_cog_check_allowed(mocker: MockerFixture):
     mock_role.id = 338173415527677954
     mock_ctx.author.roles = [mock_role]
     mock_bot = mocker.Mock(spec=commands.Bot)
+    mock_bot.localizer_loader = FluentResourceLoader("i18n/{locale}")
+    mock_ctx.bot = mock_bot
     cog = dice.Roll(mock_bot)
     assert cog.cog_check(mock_ctx) is True
 
