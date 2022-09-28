1c3e777b8f569d957374d245da50f0ec4ed1a10d
Bluesy1 <68259537+Bluesy1@users.noreply.github.com>
2022-09-28 10:16:21 -0700

    Setup a proper tetsing system for pools

---
+++ tests/charbot/conftest.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import pathlib
+
+import asyncpg
+import asyncpg.cluster
+import pytest
+import pytest_asyncio
+
+
+@pytest.fixture(scope="session")
+def cluster() -> asyncpg.cluster.TempCluster:
+    """Create the temp database cluster"""
+    test_cluster = asyncpg.cluster.TempCluster()
+    test_cluster.init()
+    test_cluster.start(port="dynamic")
+    yield test_cluster
+    cluster.stop()
+
+
+@pytest_asyncio.fixture
+async def database(cluster) -> asyncpg.Pool:
+    """Create a database pool for a test."""
+    pool = await asyncpg.create_pool(**cluster.get_connection_spec(), database="postgres")
+    with open(pathlib.Path(__file__).parent.parent.parent / "schema.sql", "r") as schema:
+        await pool.execute(schema.read())
+    await pool.execute("INSERT INTO users (id, points) VALUES (001, 50)")
+    yield pool
+    await pool.close()
