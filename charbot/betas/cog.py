45b6356d4f73b285a700abc2b6a239b8ccb93745
Bluesy1 <68259537+Bluesy1@users.noreply.github.com>
2022-10-14 08:12:29 -0700

    Disable rep cost for banner trigger while in testing

--- charbot/betas/cog.py
+++ charbot/betas/cog.py
@@ -67,7 +67,7 @@ class Betas(commands.Cog):
                         utils.utcnow() + datetime.timedelta(days=7),
                         member.id,
                     )
-                    await conn.execute("UPDATE users SET points = points - 50 WHERE id = $1", member.id)
+                    # await conn.execute("UPDATE users SET points = points - 50 WHERE id = $1", member.id)
 
     @banner.command()  # pyright: ignore[reportGeneralTypeIssues]
     @app_commands.checks.cooldown(2, 60 * 60 * 24 * 7 * 4, key=lambda i: i.user.id)
