383c6030d230b80665838a0b4395ea60a1b750ad
Bluesy <68259537+Bluesy1@users.noreply.github.com>
2022-10-18 09:31:17 -0700

    Refractor programs cog to be in its own file (#235)
    * Start working on #232
    
    * Add tests, fix regex for sudoku
    
    * Remove unused variable from tuple unpacking
    
    * Fix type checking

---
+++ charbot/programs/cog.py
@@ -0,0 +1,232 @@
+# -*- coding: utf-8 -*-
+# SPDX-FileCopyrightText: 2022 Bluesy1 <68259537+Bluesy1@users.noreply.github.com>
+#
+# SPDX-License-Identifier: MIT
+"""Program classes and functions."""
+import asyncio
+import datetime
+import random
+import re
+from itertools import count
+from typing import Final, Literal, cast
+
+import discord
+from discord import app_commands
+from discord.ext import commands
+
+from .. import CBot, GuildInteraction as Interaction, errors
+from . import sudoku, tictactoe, shrugman
+from .minesweeper import Minesweeper
+from charbot_rust.minesweeper import Game as MinesweeperGame  # pyright: ignore[reportGeneralTypeIssues]
+
+MESSAGE: Final = "You must be at least level 1 to participate in the giveaways system and be in <#969972085445238784>."
+
+
+class Reputation(commands.Cog, name="Programs"):
+    """Programs."""
+
+    def __init__(self, bot: "CBot"):  # pragma: no cover
+        self.bot = bot
+        self.sudoku_regex = re.compile(
+            r"VALUE=\"(?P<solution>\d{81})\">.*VALUE=\"(?P<mask>[01]{81})\">",
+            re.RegexFlag.MULTILINE | re.RegexFlag.DOTALL | re.RegexFlag.IGNORECASE,
+        )
+
+    async def interaction_check(self, interaction: Interaction):  # skipcq: PYL-W0221
+        """Check if the user is allowed to use the cog."""
+        if interaction.guild is None:
+            raise app_commands.NoPrivateMessage("Programs can't be used in direct messages.")
+        if interaction.guild.id != 225345178955808768:
+            raise app_commands.NoPrivateMessage("Programs can't be used in this server.")
+        channel = cast(discord.abc.GuildChannel, interaction.channel)
+        if channel.id == 839690221083820032:  # pragma: no cover
+            return True
+        if (
+            channel.id != self.bot.CHANNEL_ID
+            and interaction.command.name != self.query_points.name  # pyright: ignore[reportOptionalMemberAccess]
+        ):
+            raise errors.WrongChannelError(self.bot.CHANNEL_ID, interaction.locale)
+        if all(role.id not in self.bot.ALLOWED_ROLES for role in cast(discord.Member, interaction.user).roles):
+            raise errors.MissingProgramRole(self.bot.ALLOWED_ROLES, interaction.locale)
+        return True
+
+    programs = app_commands.Group(name="programs", description="Programs to gain you rep.", guild_only=True)
+    beta = app_commands.Group(name="beta", description="Beta programs..", parent=programs)
+
+    @programs.command(name="sudoku", description="Play a Sudoku puzzle")  # pyright: ignore[reportGeneralTypeIssues]
+    async def sudoku(self, interaction: Interaction["CBot"], mobile: bool):
+        """Generate a sudoku puzzle.
+
+        Parameters
+        ----------
+        interaction: Interaction[CBot]
+            The interaction of the command.
+        mobile: bool
+            Whether to turn off formatting that only works on desktop.
+        """
+        await interaction.response.defer(ephemeral=True)
+        async with self.bot.session.get("https://nine.websudoku.com/?level=2") as response:
+            match = self.sudoku_regex.search(str(await response.content.read()))
+            if match is None:
+                await interaction.followup.send("Couldn't find a puzzle.")
+                return
+            vals, hidden = match.group("solution", "mask")
+        board: list[list[int]] = [[] for _ in range(9)]
+        for i, num, hidden_bit in zip(count(), vals, hidden):
+            board[i // 9].append(int(num) if int(hidden_bit) == 0 else 0)
+        view = sudoku.Sudoku(sudoku.Puzzle(board, mobile), cast(discord.Member, interaction.user), self.bot)
+        await interaction.followup.send(embed=view.block_choose_embed(), view=view)
+
+    @programs.command(
+        name="tictactoe", description="Play a game of Tic Tac Toe!"
+    )  # pyright: ignore[reportGeneralTypeIssues]
+    async def tictactoe(self, interaction: Interaction["CBot"], difficulty: tictactoe.Difficulty):
+        """Play a game of Tic Tac Toe! Now built with rust.
+
+        Parameters
+        ----------
+        interaction: Interaction[CBot]
+            The interaction of the command.
+        difficulty: tictactoe.Difficulty
+            The difficulty of the game.
+        """
+        await interaction.response.defer(ephemeral=True)
+        view = tictactoe.TicTacToe(difficulty)
+        embed = discord.Embed(title="TicTacToe").set_image(url="attachment://tictactoe.png")
+        embed.set_footer(text="Play by typing /programs tictactoe")
+        image = await asyncio.to_thread(view.display)
+        await interaction.followup.send(embed=embed, view=view, file=image)
+
+    @programs.command(
+        name="shrugman", description="Play the shrugman minigame. (Hangman clone)"
+    )  # pyright: ignore[reportGeneralTypeIssues]
+    async def shrugman(self, interaction: Interaction["CBot"]) -> None:
+        """Play a game of Shrugman.
+
+        This game is a hangman-like game.
+
+        The game is played by guessing letters.
+
+        The game ends when the word is guessed or the player runs out of guesses.
+
+        The game is won by guessing the word.
+
+        The game is lost by running out of guesses.
+
+        Parameters
+        ----------
+        interaction: Interaction[CBot]
+            The interaction of the command.
+        """
+        await interaction.response.defer(ephemeral=True)
+        word = random.choice(shrugman.words)
+        embed = discord.Embed(
+            title="Shrugman",
+            description=f"Guess the word: `{''.join(['-' for _ in word])}`",
+            color=discord.Color.dark_purple(),
+        )
+        embed.set_footer(text="Play by typing /programs shrugman")
+        embed.set_author(name=interaction.user.display_name, icon_url=interaction.user.display_avatar.url)
+        view = shrugman.Shrugman(self.bot, word)
+        await interaction.followup.send(embed=embed, view=view)
+
+    @programs.command()  # pyright: ignore[reportGeneralTypeIssues]
+    async def minesweeper(
+        self,
+        interaction: Interaction["CBot"],
+        difficulty: Literal["Beginner", "Intermediate", "Expert", "Super Expert"],
+    ):
+        """Play a game of Minesweeper.
+
+        Parameters
+        ----------
+        interaction: Interaction[CBot]
+            The interaction of the command.
+        difficulty: {"Beginner", "Intermediate", "Expert", "Super Expert"}
+            The difficulty of the game.
+        """
+        await interaction.response.defer(ephemeral=True)
+        if difficulty == "Beginner":
+            game = MinesweeperGame.beginner()
+        elif difficulty == "Intermediate":
+            game = MinesweeperGame.intermediate()
+        elif difficulty == "Expert":
+            game = MinesweeperGame.expert()
+        else:
+            game = MinesweeperGame.super_expert()
+        view = Minesweeper(game)
+        file = await view.draw(await interaction.client.translate("minesweeper-lose-title", interaction.locale))
+        embed = discord.Embed(title="Minesweeper", color=discord.Color.dark_purple())
+        embed.set_footer(text="Play by typing /programs minesweeper")
+        embed.set_author(name=interaction.user.display_name, icon_url=interaction.user.display_avatar.url)
+        embed.set_image(url="attachment://minesweeper.png")
+        await interaction.followup.send(embed=embed, view=view, file=file)
+
+    # noinspection SpellCheckingInspection
+    @app_commands.command()  # pyright: ignore[reportGeneralTypeIssues]
+    @app_commands.guild_only()
+    async def rollcall(self, interaction: Interaction["CBot"]):
+        """Claim your daily reputation bonus.
+
+        Parameters
+        ----------
+        interaction: Interaction[CBot]
+            The interaction of the command invocation.
+        """
+        await interaction.response.defer(ephemeral=True)
+        giveaway_user = await self.bot.giveaway_user(interaction.user.id)
+        if giveaway_user is None:
+            async with self.bot.pool.acquire() as conn:
+                await conn.execute("INSERT INTO users (id, points) VALUES ($1, 20)", interaction.user.id)
+                await conn.execute(
+                    "INSERT INTO daily_points (id, last_claim, last_particip_dt, particip, won) VALUES "
+                    "($1, $2, $3, 0, 0)",
+                    interaction.user.id,
+                    self.bot.TIME(),
+                    self.bot.TIME() - datetime.timedelta(days=1),
+                )
+                await conn.execute("INSERT INTO bids (id, bid) VALUES ($1, 0)", interaction.user.id)
+                await interaction.followup.send("You got some Rep today, inmate")
+            return
+        if giveaway_user["daily"] >= self.bot.TIME():
+            await interaction.followup.send("No more Rep for you yet, get back to your cell")
+            return
+        async with self.bot.pool.acquire() as conn:
+            await conn.execute("UPDATE users SET points = points + 20 WHERE id = $1", interaction.user.id)
+            await conn.execute(
+                "UPDATE daily_points SET last_claim = $1 WHERE id = $2", self.bot.TIME(), interaction.user.id
+            )
+        await interaction.followup.send("You got some Rep today, inmate")
+
+    @app_commands.command(
+        name="reputation", description="Check your reputation"
+    )  # pyright: ignore[reportGeneralTypeIssues]
+    @app_commands.guild_only()
+    async def query_points(self, interaction: Interaction["CBot"]):
+        """Query your reputation.
+
+        Parameters
+        ----------
+        interaction: Interaction[CBot]
+            The interaction of the command invocation.
+        """
+        await interaction.response.defer(ephemeral=True)
+        async with self.bot.pool.acquire() as conn:
+            points = await conn.fetchval("SELECT points from users where id = $1", interaction.user.id) or 0
+            # noinspection SpellCheckingInspection
+            limits = await conn.fetchrow("SELECT * from daily_points where id = $1", interaction.user.id) or {
+                "last_claim": 0,
+                "last_particip_dt": 0,
+                "particip": 0,
+            }
+            wins = await conn.fetchval("SELECT wins FROM winners WHERE id = $1", interaction.user.id) or 0
+        claim = "have" if limits["last_claim"] == self.bot.TIME() else "haven't"
+        # noinspection SpellCheckingInspection
+        particip = (
+            "have" if (limits["last_particip_dt"] == self.bot.TIME()) and (limits["particip"] >= 10) else "haven't"
+        )
+        await interaction.followup.send(
+            f"You have {points} reputation, you {claim} claimed your daily bonus, and you {particip} hit"
+            f" your daily program cap, and have {wins}/3 wins in the last month.",
+            ephemeral=True,
+        )
--- tests/charbot/test_programs.py
+++ tests/charbot/test_programs.py
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
 # SPDX-FileCopyrightText: 2021 Bluesy1 <68259537+Bluesy1@users.noreply.github.com>
+# SPDX-FileCopyrightText: 2022 Bluesy1 <68259537+Bluesy1@users.noreply.github.com>
+#
 # SPDX-License-Identifier: MIT
+import aiohttp
 import discord
 import pytest
+from aioresponses import aioresponses
+from asyncpg import Pool
 from pytest_mock import MockerFixture
 
 from charbot import CBot
-from charbot import errors, programs
-
+from charbot import errors
+from charbot.programs.cog import Reputation
 
 pytestmark = pytest.mark.asyncio
 
@@ -21,20 +26,11 @@ def mock_bot(mocker: MockerFixture):
     return mock_bot
 
 
-async def test_programs_init(mock_bot):
-    """Test the initialization of the programs' module."""
-    cog = programs.Reputation(mock_bot)
-    assert cog.bot is mock_bot
-    assert cog.sudoku_regex.pattern == r"(\d{81}).*([01]{81})"
-    await cog.cog_unload()
-
-
 async def test_interaction_check_no_guild(mock_bot, mocker: MockerFixture):
     """Test the interaction check when the interaction is not in a guild."""
-    cog = programs.Reputation(mock_bot)
     mock_interaction = mocker.AsyncMock(spec=discord.Interaction)
     mock_interaction.guild = None
-    cog = programs.Reputation(mock_bot)
+    cog = Reputation(mock_bot)
     with pytest.raises(discord.app_commands.NoPrivateMessage) as exc:
         await cog.interaction_check(mock_interaction)
     assert exc.value.args[0] == "Programs can't be used in direct messages."
@@ -42,7 +38,7 @@ async def test_interaction_check_no_guild(mock_bot, mocker: MockerFixture):
 
 async def test_interaction_check_wrong_guild(mock_bot, mocker: MockerFixture):
     """Test the interaction check when the interaction is in the wrong guild."""
-    cog = programs.Reputation(mock_bot)
+    cog = Reputation(mock_bot)
     mock_interaction = mocker.AsyncMock(spec=discord.Interaction)
     mock_interaction.guild = mocker.AsyncMock(spec=discord.Guild)
     mock_interaction.guild.id = 0
@@ -53,7 +49,7 @@ async def test_interaction_check_wrong_guild(mock_bot, mocker: MockerFixture):
 
 async def test_interaction_check_wrong_channel(mock_bot, mocker: MockerFixture):
     """Test the interaction check when the interaction is in the wrong channel."""
-    cog = programs.Reputation(mock_bot)
+    cog = Reputation(mock_bot)
     mock_interaction = mocker.AsyncMock(spec=discord.Interaction)
     mock_interaction.guild = mocker.AsyncMock(spec=discord.Guild)
     mock_interaction.guild.id = 225345178955808768
@@ -67,7 +63,7 @@ async def test_interaction_check_wrong_channel(mock_bot, mocker: MockerFixture):
 
 async def test_interaction_check_no_allowed_roles(mock_bot, mocker: MockerFixture):
     """Test the interaction check when the interaction is in the wrong channel."""
-    cog = programs.Reputation(mock_bot)
+    cog = Reputation(mock_bot)
     mock_interaction = mocker.AsyncMock(spec=discord.Interaction)
     mock_interaction.guild = mocker.AsyncMock(spec=discord.Guild)
     mock_interaction.guild.id = 225345178955808768
@@ -101,7 +97,7 @@ async def test_interaction_check_no_allowed_roles(mock_bot, mocker: MockerFixtur
 
 async def test_interaction_check_allowed(mock_bot, mocker: MockerFixture):
     """Test the interaction check when the interaction is in the right channel and guild."""
-    cog = programs.Reputation(mock_bot)
+    cog = Reputation(mock_bot)
     mock_interaction = mocker.AsyncMock(spec=discord.Interaction)
     mock_interaction.guild = mocker.AsyncMock(spec=discord.Guild)
     mock_interaction.guild.id = 225345178955808768
@@ -113,3 +109,129 @@ async def test_interaction_check_allowed(mock_bot, mocker: MockerFixture):
         discord.Object(id=685331877057658888),
     ]
     assert await cog.interaction_check(mock_interaction)
+
+
+async def test_sudoku_command_no_puzzle(mock_bot, mocker: MockerFixture):
+    """Test that the code for the sudoku command functions as expected when no puzzle is returned."""
+    with aioresponses() as mocked:
+        async with aiohttp.ClientSession() as session:
+            mocked.get(
+                "https://nine.websudoku.com/?level=2", status=200, content_type="text/html; charset=UTF-8", body=b""
+            )
+            cog = Reputation(mock_bot)
+            mock_interaction = mocker.AsyncMock(spec=discord.Interaction)
+            mock_interaction.user = mocker.AsyncMock(spec=discord.Member)
+            mock_interaction.response = mocker.AsyncMock(spec=discord.InteractionResponse)
+            mock_interaction.followup = mocker.AsyncMock(spec=discord.Webhook)
+            mock_interaction.client = mock_bot
+            mock_interaction.client.session = session
+            await cog.sudoku.callback(cog, mock_interaction, False)  # pyright: ignore[reportGeneralTypeIssues]
+            mock_interaction.response.defer.assert_awaited_once()
+            mock_interaction.followup.send.assert_awaited_once_with("Couldn't find a puzzle.")
+
+
+async def test_sudoku_command_with_puzzle(mock_bot, mocker: MockerFixture):
+    """Test that the code for the sudoku command functions as expected when a puzzle is returned."""
+    with aioresponses() as mocked:
+        async with aiohttp.ClientSession() as session:
+            # noinspection SpellCheckingInspection
+            mocked.get(
+                "https://nine.websudoku.com/?level=2",
+                status=200,
+                content_type="text/html; charset=UTF-8",
+                body=b"""<INPUT NAME=prefix ID="prefix" TYPE=hidden VALUE="fm8jy"> <INPUT NAME=start TYPE=hidden
+                VALUE="1666103758"> <INPUT NAME=inchallenge TYPE=hidden VALUE=""> <INPUT NAME=level TYPE=hidden
+                VALUE="2"> <INPUT NAME=id ID="pid" TYPE=hidden VALUE="864770552"> <INPUT NAME=cheat ID="cheat"
+                TYPE=hidden VALUE="381642579245973816967158234429537681673481952158296347514829763796315428832764195">
+                 <INPUT ID="editmask" TYPE=hidden
+                VALUE="101111110011111000111001111110110010101010101010011011111100111000111110011111101"> <INPUT
+                NAME=options TYPE=hidden VALUE="2"> <INPUT NAME=errors TYPE=hidden VALUE="0" ID="errors"> <INPUT
+                NAME=layout TYPE=hidden VALUE=""> """,
+            )
+            cog = Reputation(mock_bot)
+            mock_interaction = mocker.AsyncMock(spec=discord.Interaction)
+            mock_interaction.user = mocker.AsyncMock(spec=discord.Member)
+            mock_interaction.response = mocker.AsyncMock(spec=discord.InteractionResponse)
+            mock_interaction.followup = mocker.AsyncMock(spec=discord.Webhook)
+            mock_interaction.client = mock_bot
+            mock_interaction.client.session = session
+            await cog.sudoku.callback(cog, mock_interaction, False)  # pyright: ignore[reportGeneralTypeIssues]
+            mock_interaction.response.defer.assert_awaited_once()
+            mock_interaction.followup.send.assert_awaited_once()
+            _, kwargs = mock_interaction.followup.send.await_args
+            assert "embed" in kwargs, "Expected an embed to be sent."
+            assert "view" in kwargs, "Expected a view to be sent."
+
+
+async def test_tictactoe_command(mock_bot, mocker: MockerFixture):
+    """Test that the code for the tictactoe command functions as expected."""
+    cog = Reputation(mock_bot)
+    mock_interaction = mocker.AsyncMock(spec=discord.Interaction)
+    mock_interaction.user = mocker.AsyncMock(spec=discord.Member)
+    mock_interaction.response = mocker.AsyncMock(spec=discord.InteractionResponse)
+    mock_interaction.followup = mocker.AsyncMock(spec=discord.Webhook)
+    mock_interaction.client = mock_bot
+    await cog.tictactoe.callback(cog, mock_interaction, 1)  # pyright: ignore[reportGeneralTypeIssues]
+    mock_interaction.response.defer.assert_awaited_once()
+    mock_interaction.followup.send.assert_awaited_once()
+    _, kwargs = mock_interaction.followup.send.await_args
+    assert "embed" in kwargs, "Expected an embed to be sent."
+    assert kwargs["embed"].image.url == "attachment://tictactoe.png", "Expected an attachment reference to be sent."
+    assert "view" in kwargs, "Expected a view to be sent."
+    assert "file" in kwargs, "Expected a file to be sent."
+
+
+async def test_shrugman_command(mock_bot, mocker: MockerFixture):
+    """Test that the code for the shrugman command functions as expected"""
+    cog = Reputation(mock_bot)
+    mock_interaction = mocker.AsyncMock(spec=discord.Interaction)
+    mock_interaction.user = mocker.AsyncMock(spec=discord.Member)
+    mock_interaction.response = mocker.AsyncMock(spec=discord.InteractionResponse)
+    mock_interaction.followup = mocker.AsyncMock(spec=discord.Webhook)
+    mock_interaction.client = mock_bot
+    await cog.shrugman.callback(cog, mock_interaction)  # pyright: ignore[reportGeneralTypeIssues]
+    mock_interaction.response.defer.assert_awaited_once()
+    mock_interaction.followup.send.assert_awaited_once()
+    _, kwargs = mock_interaction.followup.send.await_args
+    assert "embed" in kwargs, "Expected an embed to be sent."
+    assert "view" in kwargs, "Expected a view to be sent."
+
+
+@pytest.mark.parametrize("difficulty", ["Beginner", "Intermediate", "Expert", "Super Expert"])
+async def test_minesweeper_command(mock_bot, mocker: MockerFixture, difficulty: str):
+    """Test that the code for the minesweeper command functions as expected."""
+    cog = Reputation(mock_bot)
+    mock_interaction = mocker.AsyncMock(spec=discord.Interaction)
+    mock_interaction.user = mocker.AsyncMock(spec=discord.Member)
+    mock_interaction.response = mocker.AsyncMock(spec=discord.InteractionResponse)
+    mock_interaction.followup = mocker.AsyncMock(spec=discord.Webhook)
+    mock_interaction.client = mock_bot
+    await cog.minesweeper.callback(cog, mock_interaction, difficulty)  # pyright: ignore[reportGeneralTypeIssues]
+    mock_interaction.response.defer.assert_awaited_once()
+    mock_interaction.followup.send.assert_awaited_once()
+    _, kwargs = mock_interaction.followup.send.await_args
+    assert "embed" in kwargs, "Expected an embed to be sent."
+    assert kwargs["embed"].image.url == "attachment://minesweeper.png", "Expected an attachment reference to be sent."
+    assert "view" in kwargs, "Expected a view to be sent."
+    assert "file" in kwargs, "Expected a file to be sent."
+
+
+async def test_reputation_command(mock_bot, mocker: MockerFixture, database: Pool):
+    """Test that the code for the reputation command functions as expected."""
+    cog = Reputation(mock_bot)
+    mock_bot.pool = database
+    mock_interaction = mocker.AsyncMock(spec=discord.Interaction)
+    mock_interaction.user = mocker.AsyncMock(spec=discord.Member)
+    mock_interaction.user.id = 1
+    mock_interaction.response = mocker.AsyncMock(spec=discord.InteractionResponse)
+    mock_interaction.followup = mocker.AsyncMock(spec=discord.Webhook)
+    mock_interaction.client = mock_bot
+    await cog.query_points.callback(cog, mock_interaction)  # pyright: ignore[reportGeneralTypeIssues]
+    mock_interaction.response.defer.assert_awaited_once()
+    mock_interaction.followup.send.assert_awaited_once()
+    args, _ = mock_interaction.followup.send.await_args
+    assert (
+        args[0]
+        == "You have 50 reputation, you haven't claimed your daily bonus, and you haven't hit your daily program cap,"
+        " and have 0/3 wins in the last month."
+    ), "Expected the correct number of points to be sent."
