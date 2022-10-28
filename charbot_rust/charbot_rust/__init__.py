c14e1e40d113f8cf4df0e2b9d95a4ad046771b26
Bluesy1 <68259537+Bluesy1@users.noreply.github.com>
2022-10-28 10:25:14 -0700

    Fix charbot_rust typings

--- charbot_rust/charbot_rust/__init__.py
+++ charbot_rust/charbot_rust/__init__.py
@@ -2,7 +2,7 @@
 # SPDX-FileCopyrightText: 2021 Bluesy1 <68259537+Bluesy1@users.noreply.github.com>
 # SPDX-License-Identifier: MIT
 # noinspection PyUnresolvedReferences
-from .charbot_rust import *  # pyright: ignore[reportMissingImports] # noqa: F403,F401
+from .charbot_rust import *  # pyright: ignore # noqa: F403,F401
 
 # noinspection PyUnresolvedReferences
 from . import charbot_rust
