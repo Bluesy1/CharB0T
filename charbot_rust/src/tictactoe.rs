a3403f35fbd78a9b9a1d5246b2a10d29a653b1f6
Bluesy <68259537+Bluesy1@users.noreply.github.com>
2022-11-03 07:51:14 -0700

    Fixup rust codecov (#267)

--- charbot_rust/src/tictactoe.rs
+++ charbot_rust/src/tictactoe.rs
@@ -1,4 +1,5 @@
-// SPDX-FileCopyrightText: 2021 Bluesy1 <68259537+Bluesy1@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2021 Bluesy1 <68259537+Bluesy1@users.noreply.github.com>  // COV_EXCL_LINE
+//
 // SPDX-License-Identifier: MIT
 mod board; // COV_EXCL_LINE
 mod player;
