9a0fbfcf16e7e5d10d97bc8e12fd3c4a4078ae5a
Bluesy1 <68259537+Bluesy1@users.noreply.github.com>
2023-01-18 18:51:24 -0800

    Fix failing tests

--- tests/charbot/test_query.py
+++ tests/charbot/test_query.py
@@ -105,7 +105,7 @@ async def test_rule_command(mocker: MockerFixture, rule: int | None, member_id: 
         res
         == {
             1: "**1**: Be respectful to others. Being drunk is not an excuse for being stupid. Common-sense matters"
-            ". Just because it isn’t explicitly written here, doesn't mean it doesn't break the rules.\n**2**: Please"
+            ". Just because it isn't explicitly written here, doesn't mean it doesn't break the rules.\n**2**: Please"
             " utilize the text and voice channels as they are described and keep things on topic. Please be open to"
             " explaining and including others for any conversation you have here.\n**3**: Don't spam. Intentionally"
             " spamming gets you kicked or muted. Whether or not you are spamming is subject to interpretation of"
@@ -118,12 +118,12 @@ async def test_rule_command(mocker: MockerFixture, rule: int | None, member_id: 
             " and nothing X rated. Insta-ban for anyone posting pornography, etc.\n**8**: Discord invites"
             " and promotions of your own content anywhere but <#298485559813210115> are restricted, if you wish"
             " to post one somewhere please check with Mods\n**9**: Please respect the Mods here. They are good people"
-            " who sincerely want this environment to be awesome too. If they’re advising you to change your behavior,"
-            " it’s the same as me doing it. Please listen.\n**10**: Some language will be deleted. I like the idea of"
+            " who sincerely want this environment to be awesome too. If they're advising you to change your behavior,"
+            " it's the same as me doing it. Please listen.\n**10**: Some language will be deleted. I like the idea of"
             " maintaining a PG-13 community environment. Find a way to say it another way, if the bot kills your"
             " message.",
             2: "<@1>:\n**1**: Be respectful to others. Being drunk is not an excuse for being stupid. Common-sense "
-            "matters. Just because it isn’t explicitly written here, doesn't mean it doesn't break the rules.\n**2**: "
+            "matters. Just because it isn't explicitly written here, doesn't mean it doesn't break the rules.\n**2**: "
             "Please utilize the text and voice channels as they are described and keep things on topic. Please be open"
             " to explaining and including others for any conversation you have here.\n**3**: Don't spam. Intentionally"
             " spamming gets you kicked or muted. Whether or not you are spamming is subject to interpretation of"
@@ -136,16 +136,16 @@ async def test_rule_command(mocker: MockerFixture, rule: int | None, member_id: 
             " and nothing X rated. Insta-ban for anyone posting pornography, etc.\n**8**: Discord invites"
             " and promotions of your own content anywhere but <#298485559813210115> are restricted, if you wish"
             " to post one somewhere please check with Mods\n**9**: Please respect the Mods here. They are good people"
-            " who sincerely want this environment to be awesome too. If they’re advising you to change your behavior,"
-            " it’s the same as me doing it. Please listen.\n**10**: Some language will be deleted. I like the idea of"
+            " who sincerely want this environment to be awesome too. If they're advising you to change your behavior,"
+            " it's the same as me doing it. Please listen.\n**10**: Some language will be deleted. I like the idea of"
             " maintaining a PG-13 community environment. Find a way to say it another way, if the bot kills your"
             " message.",
             3: "**Rule 1** is Be respectful to others. Being drunk is not an excuse for being stupid. "
-            "Common-sense matters. Just because it isn’t explicitly written here, doesn't mean it doesn't break the "
+            "Common-sense matters. Just because it isn't explicitly written here, doesn't mean it doesn't break the "
             "rules.\n The rules can be found here: "
             "<https://cpry.net/DiscordRules>",
             4: "<@1>:\n**Rule 1** is Be respectful to others. Being drunk is not an excuse for being stupid. "
-            "Common-sense matters. Just because it isn’t explicitly written here, doesn't mean it doesn't break the "
+            "Common-sense matters. Just because it isn't explicitly written here, doesn't mean it doesn't break the "
             "rules.\n The rules can be found here: "
             "<https://cpry.net/DiscordRules>",
         }[expected_key]
