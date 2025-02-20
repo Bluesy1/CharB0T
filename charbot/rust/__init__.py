f8363bd88f21931b4148b8aa029d55e1aba359f3
Bluesy1 <68259537+Bluesy1@users.noreply.github.com>
2025-02-20 12:55:26 -0800

    Fix more things

--- charbot/rust/__init__.py
+++ charbot/rust/__init__.py
@@ -1,14 +1,4 @@
-from importlib import metadata as _metadata
-
-from . import _rust, minesweeper, tictactoe
+from . import minesweeper, tictactoe
 
 
-__doc__ = _rust.__doc__
-__title__ = "charbot_rust"
-__author__ = "Bluesy1"
-__license__ = "MIT"
-__copyright__ = "Copyright 2022-present Bluesy1"
-__version__ = _metadata.version(__title__)
 __all__ = ("tictactoe", "minesweeper")
-
-del _rust
