6b298c204980fb6f62304015feb932346ea556e2
Bluesy1 <68259537+Bluesy1@users.noreply.github.com>
2025-04-22 20:55:44 -0700

    [no ci] cleanups

--- charbot/mod_support.py
+++ charbot/mod_support.py
@@ -343,12 +343,8 @@ class ModSupportModal(ui.Modal, title="Mod Support Form"):
         topic = self.short_description.value
         assert isinstance(category, discord.CategoryChannel)  # skipcq: BAN-B101
         assert isinstance(guild, discord.Guild)  # skipcq: BAN-B101
-        assert isinstance(topic, str)  # skipcq: BAN-B101
         channel = await guild.create_text_channel(
-            self.channel_name,
-            category=category,
-            overwrites=self.perm_overrides,
-            topic=topic,
+            self.channel_name, category=category, overwrites=self.perm_overrides, topic=topic
         )
         long = "     They supplied a longer description: "
         await channel.send(
