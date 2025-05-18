579c8fa7ed22587127f791d07c06cac09ced0f0e
Bluesy1 <68259537+Bluesy1@users.noreply.github.com>
2025-05-18 12:30:34 -0700

    Update ``admin`` to have tag moderation commands

--- charbot/admin.py
+++ charbot/admin.py
@@ -1,23 +1,27 @@
 """Admin commands for the reputation system."""
 
+from datetime import timedelta
 from typing import cast
 
 import discord
 from discord import Interaction, app_commands
 from discord.ext import commands
-from discord.utils import utcnow
+from discord.utils import format_dt, utcnow
 
 from . import CBot, constants
 
 
-_ALLOWED_MENTIONS = discord.AllowedMentions(roles=False, users=False, everyone=False)
+SEVEN_DAYS = timedelta(days=7)
+MOD_ROLE = discord.Object(338173415527677954)
 
 
 @app_commands.default_permissions(manage_messages=True)
-@app_commands.checks.has_any_role(225413350874546176, 253752685357039617, 725377514414932030, 338173415527677954)
+@app_commands.checks.has_any_role(*constants.MOD_ROLE_IDS)
 @app_commands.guilds(constants.GUILD_ID)
 class ReputationAdmin(
-    commands.GroupCog, group_name="admin", group_description="Administration commands for the reputation system."
+    commands.GroupCog,
+    group_name="admin",
+    group_description="Administration commands for the reputation system.",
 ):
     """Reputation Admin Commands.
 
@@ -31,162 +35,114 @@ class ReputationAdmin(
 
     def __init__(self, bot: CBot):
         self.bot = bot
-        self.ctx_menu = app_commands.ContextMenu(
-            name="Check User's Reputation", callback=self.check_reputation_context, guild_ids=constants.GUILD_IDS
-        )
-        self.bot.tree.add_command(self.ctx_menu)
-        self._allowed_roles: list[int | str] = [
-            225413350874546176,
-            253752685357039617,
-            725377514414932030,
-            338173415527677954,
-        ]
 
-    @property
-    def allowed_roles(self) -> list[int | str]:
-        """Allow roles."""
-        return self._allowed_roles
-
-    async def cog_unload(self) -> None:
-        """Unload the cog."""
-        self.bot.tree.remove_command(self.ctx_menu.name, type=self.ctx_menu.type)
-
-    reputation = app_commands.Group(
-        name="reputation",
-        description="Administration commands for the reputation system.",
+    tags = app_commands.Group(
+        name="tags",
+        description="Administration commands for moderating guild tags.",
         guild_ids=constants.GUILD_IDS,
     )
     levels = app_commands.Group(
-        name="levels", description="Administration commands for the leveling system.", guild_ids=constants.GUILD_IDS
+        name="levels",
+        description="Administration commands for the leveling system.",
+        guild_ids=constants.GUILD_IDS,
     )
 
-    async def interaction_check(self, interaction: Interaction[CBot]) -> bool:
-        """Check if the interaction is allowed.
+    @tags.command()
+    async def warn(
+        self,
+        interaction: Interaction[CBot],
+        member: discord.Member,
+        tag: app_commands.Range[str, 4, 4],
+    ):
+        """Sends a user a warning that they have an unacceptable server tag active.
 
         Parameters
         ----------
         interaction : Interaction
             The interaction object.
-
-        Returns
-        -------
-        bool
-            Whether the interaction is allowed.
+        member : discord.Member
+            The member to warn.
+        tag : app_commands.Range[str, 4, 4]
+            The server tag the user has.
         """
-        member = interaction.user
-        if not isinstance(member, discord.Member):
-            raise app_commands.NoPrivateMessage("This command can't be used in DMs.")
-        if all(role.id not in self.allowed_roles for role in member.roles):
-            raise app_commands.MissingAnyRole(self.allowed_roles)
-        return True
+        await interaction.response.defer()
+        guild = cast(discord.Guild, interaction.guild)
+        deadline = utcnow() + SEVEN_DAYS
+        warn_msg = (
+            f"Hi {member.mention}, we noticed that you have an inappropriate Server Tag active, ``{tag}``.\n"
+            f"Please remove or change your tag by {format_dt(deadline)} ({format_dt(deadline, 'R')}) "
+            f"or we will have to kick you from {guild.name}.\n"
+            "Thanks in advance for your cooperation."
+        )
+        try:
+            await member.send(warn_msg)
+        except discord.HTTPException:
+            # Cannot DM the user, blocked | left server? Create a mod support ticket instead....
+            category: discord.CategoryChannel = discord.Object(942578610336837632, type=discord.CategoryChannel)  # pyright: ignore[reportAssignmentType]
+            permissions = {
+                guild.default_role: discord.PermissionOverwrite(
+                    view_channel=False, send_messages=False, read_messages=False
+                ),
+                MOD_ROLE: discord.PermissionOverwrite(view_channel=False, send_messages=False, read_messages=False),
+                member: discord.PermissionOverwrite.from_pair(
+                    discord.Permissions(139586817088), discord.Permissions.none()
+                ),
+            }
+            channel = await guild.create_text_channel(
+                f"Tag-{member.name}-mod-support", category=category, overwrites=permissions
+            )
+            await channel.send(warn_msg)
+        await interaction.followup.send(
+            f"{member.mention} has successfully been warned about their selected server tag."
+        )
 
-    @reputation.command()
-    async def add_reputation(self, interaction: Interaction[CBot], user: discord.User, amount: int):
-        """Add reputation to a user.
+    @tags.command()
+    async def kick(
+        self,
+        interaction: Interaction[CBot],
+        member: discord.Member,
+        tag: app_commands.Range[str, 4, 4],
+    ):
+        """Kick a member for having an unacceptable server tag active.
 
         Parameters
         ----------
         interaction : Interaction
             The interaction object.
-        user : discord.User
-            The user to add reputation to.
-        amount : int
-            The amount of reputation to add.
+        member : discord.Member
+            The member to kick.
+        tag : app_commands.Range[str, 4, 4]
+            The server tag the user has.
         """
-        await interaction.response.defer(ephemeral=True)
-        async with self.bot.pool.acquire() as conn, conn.transaction():
-            _user = await conn.fetchrow("SELECT * FROM users WHERE id = $1", user.id)
-            if _user is None:
-                await interaction.followup.send(f"Error: User `{user.name}` not found as active.")
-            else:
-                new_points: int = await conn.fetchval(
-                    "UPDATE users SET points = points + $1 WHERE id = $2 RETURNING points", amount, user.id
-                )
-                await interaction.followup.send(f"User `{user.name}` now has {new_points} reputation.")
-                client_user = cast(discord.ClientUser, self.bot.user)
-                await self.bot.program_logs.send(
-                    f"{user.mention} now has {new_points} reputation by {interaction.user.mention} ({amount} added).",
-                    allowed_mentions=_ALLOWED_MENTIONS,
-                    username=client_user.name,
-                    avatar_url=client_user.display_avatar.url,
-                )
-
-    @reputation.command()
-    async def remove_reputation(self, interaction: Interaction[CBot], user: discord.User, amount: int):
-        """Remove reputation from a user.
-
-        Parameters
-        ----------
-        interaction : Interaction
-            The interaction object.
-        user : discord.User
-            The user to remove reputation from.
-        amount : int
-            The amount of reputation to remove.
-        """
-        await interaction.response.defer(ephemeral=True)
-        async with self.bot.pool.acquire() as conn, conn.transaction():
-            _user = await conn.fetchrow("SELECT * FROM users WHERE id = $1", user.id)
-            if _user is None:
-                await interaction.followup.send(f"Error: User `{user.name}` not found as active.")
-            else:
-                if amount > _user["points"]:
-                    overflow = amount - _user["points"]
-                    amount = _user["points"]
-                else:
-                    overflow = 0
-                new_points: int = await conn.fetchval(
-                    "UPDATE users SET points = points - $1 WHERE id = $2 RETURNING points", amount, user.id
-                )
-                await interaction.followup.send(
-                    f"User `{user.name}` now has {new_points} reputation. {overflow} reputation overflow."
-                )
-                client_user = cast(discord.ClientUser, self.bot.user)
-                await self.bot.program_logs.send(
-                    f"{user.mention} now has {new_points} reputation by {interaction.user.mention} ({amount} removed).",
-                    allowed_mentions=_ALLOWED_MENTIONS,
-                    username=client_user.name,
-                    avatar_url=client_user.display_avatar.url,
-                )
-
-    @reputation.command()
-    async def check_reputation(self, interaction: Interaction[CBot], user: discord.User):
-        """Check a user's reputation.
-
-        Parameters
-        ----------
-        interaction : Interaction
-            The interaction object.
-        user : discord.User
-            The user to check.
-        """
-        await interaction.response.defer(ephemeral=True)
-        async with self.bot.pool.acquire() as conn:
-            _user = await conn.fetchrow("SELECT * FROM users WHERE id = $1", user.id)
-            if _user is None:
-                await interaction.followup.send(f"Error: User `{user.name}` not found as active.")
-            else:
-                await interaction.followup.send(f"User `{user.name}` has {_user['points']} reputation.")
-
-    @app_commands.default_permissions(manage_messages=True)
-    @app_commands.checks.has_any_role(225413350874546176, 253752685357039617, 725377514414932030, 338173415527677954)
-    async def check_reputation_context(self, interaction: Interaction[CBot], user: discord.User):
-        """Check a user's reputation.
-
-        Parameters
-        ----------
-        interaction : Interaction
-            The interaction object.
-        user : discord.User
-            The user to check.
-        """
-        await interaction.response.defer(ephemeral=True)
-        async with self.bot.pool.acquire() as conn:
-            _user = await conn.fetchrow("SELECT * FROM users WHERE id = $1", user.id)
-            if _user is None:
-                await interaction.followup.send(f"Error: User `{user.name}` not found as active.")
-            else:
-                await interaction.followup.send(f"User `{user.name}` has {_user['points']} reputation.")
+        await interaction.response.defer()
+        allowed = interaction.permissions.kick_members or await interaction.client.is_owner(interaction.user)
+        if not allowed:
+            await interaction.followup.send(
+                f"Cannot kick {member}: you do not have kick members and are not a bot owner."
+            )
+            return
+        guild = cast(discord.Guild, interaction.guild)
+        kick_msg = (
+            f"Hi {member.mention}, we noticed that you have an inappropriate Server Tag active, ``{tag}``.\n"
+            f"We have had to kick you from {guild.name}, feel free to rejoin once you change or remove your tag: "
+            "http://cpry.net/discord"
+        )
+        try:
+            await member.send(kick_msg)
+        except discord.HTTPException:
+            notified = False
+        else:
+            notified = True
+        try:
+            await member.kick(reason=f"User had an inappropriate tag: {tag}")
+        except discord.HTTPException:
+            await interaction.followup.send(
+                f"Failed to kick {member.mention} (id {member.id}) was kicked for their tag ``{tag}``. (Notified of kick successfully: {notified})."
+            )
+        else:
+            await interaction.followup.send(
+                f"{member} (id {member.id}) was kicked for their tag ``{tag}`` successfully. (Notified of kick successfully: {notified})."
+            )
 
     @levels.command()
     async def noxp_role(self, interaction: Interaction[CBot], role: discord.Role):
@@ -274,7 +230,11 @@ class ReputationAdmin(
                         interaction.user.avatar.url if interaction.user.avatar else interaction.user.default_avatar.url
                     ),
                 )
-                embed.add_field(name="Channels", value=", ".join(f"<#{c}>" for c in noxp["channels"]), inline=False)
+                embed.add_field(
+                    name="Channels",
+                    value=", ".join(f"<#{c}>" for c in noxp["channels"]),
+                    inline=False,
+                )
                 embed.add_field(name="Roles", value=", ".join(f"<@&{r}>" for r in noxp["roles"]), inline=False)
                 await interaction.followup.send(embed=embed)
 
